8f128c434154ee5508de48c3000a55eda436173a
Antonio Nuñez <anunezescamilla@deloitte.es>
2018-04-11 14:01:53 +0200

    [fjerbi] añadido servicios para catalogos maestros

--- src/app/_services/siga.service.ts
+++ src/app/_services/siga.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from "@angular/core";
 import {
-  HttpClient,
-  HttpHeaders,
-  HttpResponse,
-  HttpParams,
-  HttpResponseBase
+    HttpClient,
+    HttpHeaders,
+    HttpResponse,
+    HttpParams,
+    HttpResponseBase
 } from "@angular/common/http";
 import { Observable } from "rxjs/Observable";
 import { Router } from "@angular/router";
@@ -19,73 +19,79 @@ import { ConfirmationService } from "primeng/api";
 
 @Injectable()
 export class SigaServices {
-  items: MenuItem[];
-  endpoints = {
-    testDb: "db",
-    login: "login",
-    menu: "menu",
-    instituciones: "instituciones",
-    perfiles: "perfiles",
-    diccionarios: "diccionarios",
-    usuarios_rol: "usuarios/rol",
-    usuarios_perfil: "usuarios/perfil",
-    usuarios_search: "usuarios/search",
-    usuarios_delete: "usuarios/delete"
-  };
+    items: MenuItem[];
+    endpoints = {
+        testDb: "db",
+        login: "login",
+        menu: "menu",
+        instituciones: "instituciones",
+        perfiles: "perfiles",
+        diccionarios: "diccionarios",
+        usuarios_rol: "usuarios/rol",
+        usuarios_perfil: "usuarios/perfil",
+        usuarios_search: "usuarios/search",
+        usuarios_delete: "usuarios/delete",
+        maestros_search: "catmaestros/search",
+        maestros_rol: "catmaestros/tabla",
+        maestros_update: "catmaestros/update",
+        maestros_create: "catmaestros/create",
+        maestros_delete: "catmaestros/delete"
 
-  constructor(private http: HttpClient) {}
+    };
 
-  get(service: string): Observable<any> {
-    return this.http
-      .get(environment.newSigaUrl + this.endpoints[service])
-      .map(response => {
-        return response;
-      });
-  }
+    constructor(private http: HttpClient) { }
 
-  getNewSigaUrl() {
-    return environment.newSigaUrl;
-  }
+    get(service: string): Observable<any> {
+        return this.http
+            .get(environment.newSigaUrl + this.endpoints[service])
+            .map(response => {
+                return response;
+            });
+    }
 
-  getPerfil(service: string, institucion: string): Observable<any> {
-    return this.http
-      .get(
-        environment.newSigaUrl +
-          this.endpoints[service] +
-          "?institucion=" +
-          institucion
-      )
-      .map(response => {
-        return response;
-      });
-  }
+    getNewSigaUrl() {
+        return environment.newSigaUrl;
+    }
 
-  post(service: string, body: any): Observable<any> {
-    let headers = new HttpHeaders({
-      "Content-Type": "application/json"
-    });
-    return this.http
-      .post(environment.newSigaUrl + this.endpoints[service], body, {
-        headers: headers,
-        observe: "response",
-        responseType: "text"
-      })
-      .map(response => {
-        return response;
-      });
-  }
-  postPaginado(service: string, param: string, body: any): Observable<any> {
-    let headers = new HttpHeaders({
-      "Content-Type": "application/json"
-    });
-    return this.http
-      .post(environment.newSigaUrl + this.endpoints[service] + param, body, {
-        headers: headers,
-        observe: "response",
-        responseType: "text"
-      })
-      .map(response => {
-        return response;
-      });
-  }
+    getPerfil(service: string, institucion: string): Observable<any> {
+        return this.http
+            .get(
+            environment.newSigaUrl +
+            this.endpoints[service] +
+            "?institucion=" +
+            institucion
+            )
+            .map(response => {
+                return response;
+            });
+    }
+
+    post(service: string, body: any): Observable<any> {
+        let headers = new HttpHeaders({
+            "Content-Type": "application/json"
+        });
+        return this.http
+            .post(environment.newSigaUrl + this.endpoints[service], body, {
+                headers: headers,
+                observe: "response",
+                responseType: "text"
+            })
+            .map(response => {
+                return response;
+            });
+    }
+    postPaginado(service: string, param: string, body: any): Observable<any> {
+        let headers = new HttpHeaders({
+            "Content-Type": "application/json"
+        });
+        return this.http
+            .post(environment.newSigaUrl + this.endpoints[service] + param, body, {
+                headers: headers,
+                observe: "response",
+                responseType: "text"
+            })
+            .map(response => {
+                return response;
+            });
+    }
 }
