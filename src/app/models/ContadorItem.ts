f194f377f76b6f8aedb8d6fcf71df5195756411f
Antonio Nuñez <anunezescamilla@deloitte.es>
2018-04-23 13:32:13 +0200

    [anunezescamilla] modelo Contador

---
+++ src/app/models/ContadorItem.ts
@@ -0,0 +1,27 @@
+export class ContadorItem {
+  idinstitucion: String;
+  idcontador: String;
+  nombre: String;
+  descripcion: String;
+  modificablecontador: String;
+  modo: Number;
+  contador: Number;
+  prefijo: String;
+  sufijo: String;
+  longitudcontador: Number;
+  fechareconfiguracion: Date;
+  reconfiguracioncontador: String;
+  reconfiguracionprefijo: String;
+  reconfiguracionsufijo: String;
+  idtabla: String;
+  idcampocontador: String;
+  idcampoprefijo: String;
+  idcamposufijo: String;
+  general: String;
+  fechamodificacion: Date;
+  usumodificacion: Number;
+  fechacreacion: Date;
+  usucreacion: Number;
+
+  constructor() {}
+}
