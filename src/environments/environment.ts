721dacc786e87fbcd8ff03e2ae8dbba2420e9f7c
Antonio Nuñez <anunezescamilla@deloitte.es>
2018-04-26 13:40:14 +0200

    merge

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,6 +10,6 @@ export const environment = {
   baseHref: "/ang/",
   // oldSigaUrl: 'https://7plus.demo.deloitte.es/SIGA/',
   oldSigaUrl: "https://vmdxdarsap028.cloud.es.deloitte.com/SIGA/",
-  newSigaUrl: "http://localhost:8180/siga-web-0.0.1-SNAPSHOT/"
-  //newSigaUrl: "http://localhost:7001/siga-web/"
+  //newSigaUrl: "http://localhost:8180/siga-web-0.0.1-SNAPSHOT/"
+  newSigaUrl: "http://localhost:7001/siga-web/"
 };
