6bb79e2d0d279708a5ed0d3af617a94e6eb501a6
Manuel Jimenez <mjimenezmolina@deloitte.es>
2018-04-27 11:29:12 +0200

    [mjimenezmolina] Cambios en front para pantalla de auditoria de usuarios.

---
+++ src/app/models/HistoricoUsuarioItem.ts
@@ -0,0 +1,15 @@
+export class HistoricoUsuarioItem {
+  idPersona: String;
+  idHistorico: String;
+  fechaEfectiva: Date;
+  fechaEntrada: Date;
+  motivo: String;
+  descripcion: String;
+  idTipoCambio: String;
+  descTipoCambio: String;
+  nombre: String;
+  apellidos1: String;
+  apellidos2: String;
+  descripcionUsuario: String;
+  constructor() {}
+}
