36498b29b3d1e96aa4a0e608d61c2a999fca78e0
Jesús Colchón <jcolchon@deloitte.es>
2018-05-07 08:28:20 +0200

    [jcolchon] se sube el código de este fichero eliminando el nombre erróneo del Component

--- src/app/features/facturacion/mantenimiento-sufijos/mantenimiento-sufijos.component.spec.ts
+++ src/app/features/facturacion/mantenimiento-sufijos/mantenimiento-sufijos.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MantenimientoSufijosComponent } from './mantenimiento-sufijos.component';
 
-describe('ComunicacionInterprofesionalComponent', () => {
+describe('MantenimientoSufijosComponent', () => {
   let component: MantenimientoSufijosComponent;
   let fixture: ComponentFixture<MantenimientoSufijosComponent>;
 
