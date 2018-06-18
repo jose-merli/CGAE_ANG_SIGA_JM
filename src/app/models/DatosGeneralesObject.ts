284012ef51b0f705216ebff8116e6e054f4be37f
Ainara Corta <acorta@deloitte.es>
2018-06-18 13:36:20 +0200

    Tarjeta Datos Generales.

--- src/app/models/DatosGeneralesObject.ts
+++ src/app/models/DatosGeneralesObject.ts
@@ -1,7 +1,7 @@
 import { DatosGeneralesItem } from "./DatosGeneralesItem";
 import { ErrorItem } from "./ErrorItem";
 export class DatosGeneralesObject {
-  error: Error;
+  error: ErrorItem;
   personaJuridicaItems: DatosGeneralesItem[] = [];
   constructor() {}
 }
