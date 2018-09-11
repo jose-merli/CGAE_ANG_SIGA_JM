446911a4b8d22c0ea6b345a901c4b9d6f68d363e
Fredi Jerbi <fjerbi@deloitte.es>
2018-09-11 14:57:52 +0200

    [fjerbi] Agregada pantalla de Edición Datos Curriculares y DTO's

---
+++ src/app/models/FichaColegialEdicionCurricularesItem.ts
@@ -0,0 +1,12 @@
+export class FichaColegialEdicionCurricularesItem {
+  categoriaCurricular: String;
+  subtipoCurricular: String;
+  fechaDesde: Date;
+  fechaHasta: Date;
+  creditos: String;
+  verificado: Boolean;
+  fechaVerificacion: Date;
+  descripcion: String;
+
+  constructor() {}
+}
---
+++ src/app/models/FichaColegialEdicionCurricularesObject.ts
@@ -0,0 +1,7 @@
+import { FichaColegialEdicionCurricularesItem } from "./FichaColegialEdicionCurricularesItem";
+import { ErrorItem } from "./ErrorItem";
+export class FichaColegialEdicionCurricularesObject {
+  error: ErrorItem;
+  FichaColegialEdicionCurricularesItem: FichaColegialEdicionCurricularesItem[] = [];
+  constructor() {}
+}
