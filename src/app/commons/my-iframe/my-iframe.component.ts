32a798ed4c2230ccda3bdf5316c6bf57bb7c2894
Jesús Colchón <jcolchon@deloitte.es>
2018-09-11 08:47:41 +0200

    jcolchon cambios por incidencias de Siga Classique

--- src/app/commons/my-iframe/my-iframe.component.ts
+++ src/app/commons/my-iframe/my-iframe.component.ts
@@ -1,33 +1,75 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Renderer2
+} from "@angular/core";
 import { DomSanitizer } from "@angular/platform-browser";
-import { Input } from '@angular/core';
-import { OldSigaServices } from '../../_services/oldSiga.service';
+import { Input } from "@angular/core";
+import { OldSigaServices } from "../../_services/oldSiga.service";
+import { Pipe, PipeTransform } from "@angular/core";
+import { SigaServices } from "../../_services/siga.service";
 // import '../../../../SIGA.js'
 
 declare var ajusteAlto: any;
 
 @Component({
-  selector: 'app-my-iframe',
-  templateUrl: './my-iframe.component.html',
-  styleUrls: ['./my-iframe.component.scss']
+  selector: "app-my-iframe",
+  templateUrl: "./my-iframe.component.html",
+  styleUrls: ["./my-iframe.component.scss"]
 })
+@Pipe({ name: "safe" })
 export class MyIframeComponent implements OnInit {
-
-  @Input() url;
+  @Input()
+  url;
   // @ViewChild('iframe') iframe: ElementRef;
 
   // loading = true;
+  cerrojo: boolean = true;
+  @ViewChild("test")
+  test: ElementRef;
+  constructor(
+    private domSanitizer: DomSanitizer,
+    private service: OldSigaServices,
+    private renderer: Renderer2,
+    private sigaServices: SigaServices
+  ) {}
 
-  constructor(private domSanitizer: DomSanitizer, private service: OldSigaServices) { }
-
+  // transform(url: string) {
+  //   return this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+  // }
   ngOnInit() {
+    //this.renderer.removeClass();
     this.url = this.domSanitizer.bypassSecurityTrustResourceUrl(this.url);
+
+    if (
+      String(this.url).includes("Dispatcher.do") &&
+      sessionStorage.getItem("cerrojoDispatcher") === "true"
+    ) {
+      window.location.reload();
+      console.log("refresca iframe");
+      this.cerrojo = false;
+      sessionStorage.setItem("cerrojoDispatcher", "false");
+    }
+
+    this.sigaServices.iframeRemove$.subscribe(() => {
+      this.cerrojo = true;
+      sessionStorage.setItem("cerrojoDispatcher", "true");
+    });
+
+    //this.test.nativeElement.remove();
+
+    // const child = document.createElement('div');
+    // this.renderer.appendChild(this.elementRef.nativeElement, child);
+
+    //this.test.nativeElement.
+
     //ajusteAlto('mainWorkArea');
     // this.service.get(this.url).subscribe(blob => {
     //   // this.loading = false;
     //   this.iframe.nativeElement.src = blob;
     // });
   }
-
-
 }
+
