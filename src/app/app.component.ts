92cdfb220abb7b9bb943bd9546ddee1129a4507a
Salinero, Isabel (ES - Madrid) <isalinero@deloitte.es>
2018-12-13 18:29:57 +0100

    app.component OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,7 +41,7 @@ export class AppComponent implements OnInit {
 
 
     //sessionStorage.removeItem("authenticated");
-    // sessionStorage.clear();
+    sessionStorage.clear();
     this.router.routeReuseStrategy.shouldReuseRoute = function () {
       return false;
     };
