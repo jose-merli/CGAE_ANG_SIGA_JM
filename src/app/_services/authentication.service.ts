3cab649d233925cc9ac3ceb2fe7401e243a198a4
Borja Herrero <bherrero@deloitte.es>
2018-12-14 11:06:56 +0100

    Revert "GitIgnore"
    This reverts commit 9d880a40a4e21015a62ce7e4357430dd118de99f.

---
+++ src/app/_services/authentication.service.ts
@@ -0,0 +1,159 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse, HttpParams, HttpResponseBase, HttpBackend } from '@angular/common/http';
+import { URLSearchParams } from '@angular/http'
+import { Observable } from 'rxjs';
+import { forkJoin } from "rxjs/observable/forkJoin";
+import { Router } from '@angular/router';
+import 'rxjs/add/operator/map'
+import { SigaServices } from './siga.service';
+import { OldSigaServices } from './oldSiga.service';
+import { environment } from '../../environments/environment'
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+
+
+@Injectable()
+export class AuthenticationService {
+
+    private http: HttpClient;
+    private sigaServices: SigaServices;
+    private oldSigaServices: OldSigaServices;
+
+    constructor(private router: Router, handler: HttpBackend, sigaServices: SigaServices, oldSigaServices: OldSigaServices) {
+        this.http = new HttpClient(handler);
+        this.sigaServices = sigaServices;
+        this.oldSigaServices = oldSigaServices;
+    }
+
+    // url = this.globals.getBaseUrl() + "/SIGA/developmentLogin.do";
+    // urlNewSiga = this.globals.getBaseUrl() + "/siga-web/login";
+
+    logout() {
+        sessionStorage.removeItem('authenticated');
+        this.router.navigate(['/login']);
+
+        //     if (sessionStorage.getItem('loginDevelop') === 'true' ) {
+        //         this.router.navigate(["/loginDevelop"])
+        //     }else{
+        //         if (sessionStorage.getItem('loginDevelop') === '0' ) {
+        //             sessionStorage.clear();
+        //             this.router.navigate(["/loginDevelop"])
+        //         }else{
+        //         this.router.navigate(['/login']);
+        //         }
+        // }
+    }
+
+    isAutenticated() {
+        return sessionStorage.getItem('authenticated') === 'true';
+    }
+
+    isAutenticetedInOldSiga() {
+        return sessionStorage.getItem('osAutenticated') === 'true';
+    }
+
+    newSigaLogin(): Observable<any> {
+        return this.http.get(this.sigaServices.getNewSigaUrl() + this.sigaServices.endpoints["login"], { observe: 'response' });
+    }
+    newSigaDevelopLogin(formValues): Observable<any> {
+        let params = '?';
+        for (let key in formValues) {
+            params = params + key + '=' + formValues[key] + '&';
+        }
+
+        return this.http.get(this.sigaServices.getNewSigaUrl() + this.sigaServices.endpoints["loginDevelop"] + params, { observe: 'response' });
+    }
+
+    oldSigaDevelopLogin(formValues): Observable<any> {
+        var formData: FormData = new FormData();
+        for (let key in formValues) {
+            formData.append(key, formValues[key]);
+        }
+
+        //let params = new URLSearchParams();
+        let params = '?';
+        for (let key in formValues) {
+            //params.set(key, formValues[key]);
+            params = params + key + '=' + formValues[key] + '&';
+        }
+
+        let headers = new HttpHeaders({
+            'Content-Type': 'application/x-www-form-urlencoded',
+        })
+        let options = { headers: headers, observe: 'response', responseType: 'text' }
+
+        return this.http.get(this.oldSigaServices.getOldSigaUrl("loginDevelop") + params, { observe: 'response', responseType: "text" });
+
+    }
+
+    oldSigaLogin(): Observable<any> {
+
+        let headers = new HttpHeaders({
+            'Content-Type': 'application/x-www-form-urlencoded',
+        })
+        let options = { headers: headers, observe: 'response', responseType: 'text' }
+        return this.http.get(this.oldSigaServices.getOldSigaUrl("login"), { headers: headers, observe: 'response', responseType: 'text' })
+
+
+    }
+
+
+    autenticate(): Observable<any> {
+        sessionStorage.removeItem("authenticated");
+        let newSigaRquest = this.newSigaLogin();
+        let oldSigaRquest = this.oldSigaLogin();
+
+
+        return forkJoin([oldSigaRquest, newSigaRquest]).map(
+            (response) => {
+                let newSigaResponse = response[1].headers.get("Authorization");
+                let oldSigaResponse = response[0].status;
+                if (oldSigaResponse == 200) {
+                    sessionStorage.setItem('osAutenticated', 'true');
+
+                    sessionStorage.setItem('authenticated', 'true');
+                    sessionStorage.setItem('Authorization', newSigaResponse);
+
+                    return true;
+                }
+
+            }
+        )
+    }
+
+
+    autenticateDevelop(formValues): Observable<any> {
+        let newSigaRquest = this.newSigaDevelopLogin(formValues);
+
+        // return forkJoin([newSigaRquest]).map(
+        //     (response) => {
+        //         let newSigaResponse = response[0].headers.get("Authorization");
+        //         let newSigaResponseStatus = response[0].status;
+        //         if (newSigaResponseStatus == 200) {
+        //             sessionStorage.setItem('osAutenticated', 'true');
+        //             sessionStorage.setItem('authenticated', 'true');
+        //             sessionStorage.setItem('Authorization', newSigaResponse);
+        //             return true;
+        //         }
+        //     }
+        // )
+
+        let oldSigaRquest = this.oldSigaDevelopLogin(formValues);
+        return forkJoin([oldSigaRquest, newSigaRquest]).map(
+            (response) => {
+                let newSigaResponse = response[1].headers.get("Authorization");
+                let oldSigaResponse = response[0].status;
+                if (oldSigaResponse == 200) {
+                    sessionStorage.setItem('osAutenticated', 'true');
+
+                    sessionStorage.setItem('authenticated', 'true');
+                    sessionStorage.setItem('Authorization', newSigaResponse);
+
+                    return true;
+                }
+
+            }
+        )
+
+    }
+
+}
---
+++ src/app/commons/login-develop/login-develop.component.ts
@@ -0,0 +1,169 @@
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, FormControl } from "@angular/forms";
+import { AuthenticationService } from "../../_services/authentication.service";
+import { SigaServices } from "../../_services/siga.service";
+import { Router } from "@angular/router";
+import { LoginCombo } from "./login-develop.combo";
+import { ListboxModule } from "primeng/listbox";
+import { ButtonModule } from "primeng/button";
+
+@Component({
+  selector: "app-login-develop",
+  templateUrl: "./login-develop.component.html",
+  styleUrls: ["./login-develop.component.scss"]
+})
+export class LoginDevelopComponent implements OnInit {
+  form: FormGroup;
+
+  instituciones: any[];
+  perfiles: any[];
+  isLetrado: String;
+  isEntrar: boolean = true;
+  tmpLoginPerfil: String[];
+  entorno: String;
+  ocultar: boolean = false;
+  progressSpinner: boolean = false;
+  // value=N selected="">NO, no soy Letrado</option>
+  //                   <option value=S>SÍ, soy Letrado</option>
+
+  letrado: any[] = [
+    { label: "No, no soy Letrado", value: "N" },
+    { label: "Sí, soy Letrado", value: "S" }
+  ];
+  constructor(
+    private fb: FormBuilder,
+    private service: AuthenticationService,
+    private sigaServices: SigaServices,
+    private router: Router
+  ) { }
+
+  onSubmit() { }
+
+  ngOnInit() {
+    sessionStorage.removeItem("authenticated");
+    this.ocultar = true;
+    this.progressSpinner = true;
+    this.sigaServices.getBackend("validaInstitucion").subscribe(
+      response => {
+        this.progressSpinner = false;
+        this.ocultar = true;
+      },
+      error => {
+        console.log("ERROR", error);
+        if (error.status == 403) {
+          let codError = error.status;
+
+          sessionStorage.setItem("codError", codError);
+          sessionStorage.setItem("descError", "Imposible validar el certificado");
+          this.router.navigate(["/errorAcceso"]);
+          this.progressSpinner = false;
+        }
+      }
+    );
+    this.sigaServices.getBackend("instituciones").subscribe(n => {
+      this.instituciones = n.combooItems;
+
+      /*creamos un labelSinTilde que guarde los labels sin caracteres especiales, 
+para poder filtrar el dato con o sin estos caracteres*/
+      this.instituciones.map(e => {
+        let accents = 'ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž';
+        let accentsOut = "AAAAAAaaaaaaOOOOOOOooooooEEEEeeeeeCcDIIIIiiiiUUUUuuuuNnSsYyyZz";
+        let i;
+        let x;
+        for (i = 0; i < e.label.length; i++) {
+          if ((x = accents.indexOf(e.label[i])) != -1) {
+            e.labelSinTilde = e.label.replace(e.label[i], accentsOut[x]);
+            return e.labelSinTilde;
+          }
+        }
+      });
+
+      this.isLetrado = "N";
+    });
+    this.ocultar = true;
+    this.form = this.fb.group({
+      tmpLoginInstitucion: new FormControl(""),
+      tmpLoginPerfil: new FormControl("ADG"),
+      sLetrado: new FormControl("N"),
+      user: new FormControl(),
+      letrado: new FormControl("N"),
+      location: new FormControl("2000"),
+      profile: new FormControl("ADG"),
+
+      posMenu: new FormControl(0)
+    });
+    //this.onChange(this.form.controls['tmpLoginInstitucion'].value);
+    this.form.controls["tmpLoginInstitucion"].valueChanges.subscribe(
+      newValue => {
+        this.form.controls["location"].setValue(newValue);
+      }
+    );
+
+    this.form.controls["tmpLoginPerfil"].valueChanges.subscribe(n => {
+      this.form.controls["profile"].setValue(n);
+    });
+    this.form.controls["sLetrado"].valueChanges.subscribe(n => {
+      this.form.controls["letrado"].setValue(n);
+    });
+
+    // this.form.setValue({'location': this.form.value.tmpLoginInstitucion});
+  }
+
+  submit() {
+    var ir = null;
+    this.progressSpinner = true;
+    this.service.autenticateDevelop(this.form.value).subscribe(
+      response => {
+        if (response) {
+          this.router.navigate(["/home"]);
+        } else {
+          this.router.navigate(["/landpage"]);
+        }
+      },
+      error => {
+        console.log("ERROR", error);
+        if (error.status == 403) {
+          let codError = error.status;
+
+          sessionStorage.setItem("codError", codError);
+          sessionStorage.setItem("descError", "Imposible validar el certificado");
+          this.router.navigate(["/errorAcceso"]);
+        }
+      }
+    );
+  }
+
+  onChange(newValue) {
+    this.tmpLoginPerfil = ["ADG"];
+    var ir = null;
+    this.form.controls["location"].setValue(newValue.value);
+    // this.form.controls["tmpLoginInstitucion"].setValue(newValue.value);
+    this.sigaServices.getPerfil("perfiles", newValue.value).subscribe(n => {
+      this.perfiles = n.combooItems;
+    });
+    // this.tmpLoginPerfil = "Administrador General";
+    //console.log(newValue);
+    //let combo = new LoginCombo();
+    //combo.setValue(newValue.id);
+    //this.sigaServices.post("perfilespost", combo).subscribe(n => {
+    //if (n) {
+    //this.perfiles = JSON.parse(n['body']);;
+    //}
+    //});
+  }
+
+  isHabilitadoEntrar() {
+    if (
+      this.form.controls["tmpLoginPerfil"].value == "" ||
+      this.form.controls["tmpLoginPerfil"].value == undefined ||
+      (this.form.controls["tmpLoginInstitucion"].value == "" ||
+        this.form.controls["tmpLoginInstitucion"].value == undefined)
+    ) {
+      this.isEntrar = true;
+      return this.isEntrar;
+    } else {
+      this.isEntrar = false;
+      return this.isEntrar;
+    }
+  }
+}
