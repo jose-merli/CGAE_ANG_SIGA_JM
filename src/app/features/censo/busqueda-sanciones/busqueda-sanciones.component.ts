d6bf5c129bf1af7279ac550b77f94e46ab512653
José Ángel González García <jgonzalez@deloitte.es>
2019-01-17 19:17:37 +0100

    [jgonzalez] Coreccion de incidencias

--- src/app/features/censo/busqueda-sanciones/busqueda-sanciones.component.ts
+++ src/app/features/censo/busqueda-sanciones/busqueda-sanciones.component.ts
@@ -52,6 +52,8 @@ export class BusquedaSancionesComponent implements OnInit {
   body: BusquedaSancionesItem = new BusquedaSancionesItem();
   bodySearch: BusquedaSancionesObject = new BusquedaSancionesObject();
 
+  isLetrado: boolean = false;
+
   constructor(
     private sigaServices: SigaServices,
     private changeDetectorRef: ChangeDetectorRef,
@@ -103,6 +105,13 @@ export class BusquedaSancionesComponent implements OnInit {
       sessionStorage.removeItem("SancionInsertada");
     }
     console.log("array", this.colegios_seleccionados);
+
+    if (
+      sessionStorage.getItem("isLetrado") != null &&
+      sessionStorage.getItem("isLetrado") != undefined
+    ) {
+      this.isLetrado = JSON.parse(sessionStorage.getItem("isLetrado"));
+    }
   }
 
   getComboTipoSancion() {
