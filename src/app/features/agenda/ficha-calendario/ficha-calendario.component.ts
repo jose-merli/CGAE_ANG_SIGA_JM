b0d82578f9b4f7f786579af76d5003f963efaac9
María José Tubío <mtubio@deloitte.es>
2019-03-22 12:51:16 +0100

    [mtubio] Pantalla Ficha Calendario/Ficha Evento -Tarjeta Notificaciones - Arreglo al guardar una notificación sin tipo de envío

--- src/app/features/agenda/ficha-calendario/ficha-calendario.component.ts
+++ src/app/features/agenda/ficha-calendario/ficha-calendario.component.ts
@@ -725,6 +725,7 @@ export class FichaCalendarioComponent implements OnInit {
     this.sigaServices.get("datosNotificaciones_getPlantillas").subscribe(
       n => {
         this.comboTemplates = n.combooItems;
+        this.arregloTildesCombo(this.comboTemplates);
       },
       err => {
         console.log(err);
@@ -775,6 +776,7 @@ export class FichaCalendarioComponent implements OnInit {
   }
 
   getTypeSend(idPlantillaEnvio, idTipoEnvio, dato) {
+    this.progressSpinner = true;
     let typeSend = [];
     this.sigaServices
       .getParam(
@@ -785,15 +787,26 @@ export class FichaCalendarioComponent implements OnInit {
         n => {
           typeSend = n.combooItems;
 
-          if (this.newNotificacion != undefined) {
-            this.newNotificacion.idTipoEnvio = typeSend[0].value;
+          if (typeSend.length != 0) {
+
+            if (this.newNotificacion != undefined) {
+              this.newNotificacion.idTipoEnvio = typeSend[0].value;
+              this.newNotificacion.tipoEnvio = typeSend[0].label;
+            } else {
+              dato.tipoEnvio = typeSend[0].label;
+              dato.idTipoEnvio = typeSend[0].value;
+            }
+
+          } else {
+            dato.tipoEnvio = undefined;
+            dato.idTipoEnvio = undefined;
           }
-          dato.tipoEnvio = typeSend[0].label;
-          dato.idTipoEnvio = typeSend[0].value;
+
           if (!this.pressNewNotificacion) {
             this.editNotificaciones(dato);
           }
 
+          this.progressSpinner = false;
         },
         err => {
           console.log(err);
@@ -879,6 +892,7 @@ export class FichaCalendarioComponent implements OnInit {
         this.numSelectedNotifications = 0;
         this.showSuccess();
         this.updateNotificationList = [];
+        this.newNotificacion = undefined;
         this.getCalendarNotifications();
       },
       err => {
@@ -1041,6 +1055,7 @@ export class FichaCalendarioComponent implements OnInit {
       this.accesoCalendario = true;
     }
   }
+
   checkAccesoSeleccionDatos() {
     if (
       this.selectedDatosNotifications &&
@@ -1051,5 +1066,22 @@ export class FichaCalendarioComponent implements OnInit {
     } else {
       this.accesoSeleccionDatos = true;
     }
+  }
+
+  arregloTildesCombo(combo) {
+    combo.map(e => {
+      let accents =
+        "ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž";
+      let accentsOut =
+        "AAAAAAaaaaaaOOOOOOOooooooEEEEeeeeeCcDIIIIiiiiUUUUuuuuNnSsYyyZz";
+      let i;
+      let x;
+      for (i = 0; i < e.label.length; i++) {
+        if ((x = accents.indexOf(e.label[i])) != -1) {
+          e.labelSinTilde = e.label.replace(e.label[i], accentsOut[x]);
+          return e.labelSinTilde;
+        }
+      }
+    });
   }
 }
--- src/app/features/agenda/ficha-eventos/ficha-eventos.component.ts
+++ src/app/features/agenda/ficha-eventos/ficha-eventos.component.ts
@@ -1616,6 +1616,8 @@ export class FichaEventosComponent implements OnInit, OnDestroy {
       tipoEvento = "General";
     } else if (this.newEvent.idTipoEvento == this.valorTipoEventoFestivo) {
       tipoEvento = "Laboral";
+    } else if (this.newEvent.idTipoEvento == this.valorTipoEventoSesion) {
+      tipoEvento = "Sesión";
     } else {
       tipoEvento = this.newEvent.tipoEvento;
     }
@@ -1678,6 +1680,7 @@ export class FichaEventosComponent implements OnInit, OnDestroy {
         this.pressNewNotificacion = false;
         this.editNotificacion = false;
         this.selectedDatosNotifications = [];
+        this.newNotificacion = undefined;
         this.numSelectedNotification = 0;
         this.updateNotificationList = [];
         this.getEventNotifications();
@@ -1821,6 +1824,7 @@ export class FichaEventosComponent implements OnInit, OnDestroy {
     this.sigaServices.get("datosNotificaciones_getPlantillas").subscribe(
       n => {
         this.comboTemplates = n.combooItems;
+        this.arregloTildesCombo(this.comboTemplates);
       },
       err => {
         console.log(err);
@@ -1871,6 +1875,7 @@ export class FichaEventosComponent implements OnInit, OnDestroy {
   }
 
   getTypeSend(idPlantillaEnvio, idTipoEnvio, dato) {
+    this.progressSpinner = true;
     let typeSend = [];
     this.sigaServices
       .getParam(
@@ -1881,14 +1886,26 @@ export class FichaEventosComponent implements OnInit, OnDestroy {
         n => {
           typeSend = n.combooItems;
 
-          if (this.newNotificacion != undefined) {
-            this.newNotificacion.idTipoEnvio = typeSend[0].value;
+          if (typeSend.length != 0) {
+
+            if (this.newNotificacion != undefined) {
+              this.newNotificacion.idTipoEnvio = typeSend[0].value;
+              this.newNotificacion.tipoEnvio = typeSend[0].label;
+            } else {
+              dato.tipoEnvio = typeSend[0].label;
+              dato.idTipoEnvio = typeSend[0].value;
+            }
+
+          } else {
+            dato.tipoEnvio = undefined;
+            dato.idTipoEnvio = undefined;
           }
-          dato.tipoEnvio = typeSend[0].label;
-          dato.idTipoEnvio = typeSend[0].value;
+
           if (!this.pressNewNotificacion) {
             this.editNotificaciones(dato);
           }
+          this.progressSpinner = false;
+
         },
         err => {
           console.log(err);
@@ -1903,7 +1920,6 @@ export class FichaEventosComponent implements OnInit, OnDestroy {
   validateNotification() {
     if (this.newNotificacion != undefined) {
       if (
-        this.newNotificacion.idTipoEnvio == undefined ||
         this.newNotificacion.idPlantilla == undefined ||
         this.newNotificacion.idTipoCuando == undefined ||
         this.newNotificacion.idUnidadMedida == undefined ||
@@ -2482,5 +2498,22 @@ export class FichaEventosComponent implements OnInit, OnDestroy {
     } else {
       this.newEvent.idEstadoEvento = this.valorEstadoEventoPlanificado;
     }
+  }
+
+  arregloTildesCombo(combo) {
+    combo.map(e => {
+      let accents =
+        "ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž";
+      let accentsOut =
+        "AAAAAAaaaaaaOOOOOOOooooooEEEEeeeeeCcDIIIIiiiiUUUUuuuuNnSsYyyZz";
+      let i;
+      let x;
+      for (i = 0; i < e.label.length; i++) {
+        if ((x = accents.indexOf(e.label[i])) != -1) {
+          e.labelSinTilde = e.label.replace(e.label[i], accentsOut[x]);
+          return e.labelSinTilde;
+        }
+      }
+    });
   }
 }
