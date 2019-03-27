6aff5fe007e427505ac6d936b972db2c07c796cd
María José Tubío <mtubio@deloitte.es>
2019-03-27 17:00:20 +0100

    [mtubio] FASE I:  Eliminar fisicamente un perfil asignado de un usuario, en tarjeta en datos bancarios guardar el fichero adjunto a un mandato o y descargarlo correctamente -- FASE II: SIGARNV-375, SIGARNV-392, SIGARNV-430, SIGARNV-543, SIGARNV-545, SIGARNV-549, SIGARNV-550, SIGARNV-551, SIGARNV-552, SIGARNV-555

--- src/app/models/DatosBancariosSearchAnexosItem.ts
+++ src/app/models/DatosBancariosSearchAnexosItem.ts
@@ -16,5 +16,6 @@ export class DatosBancariosSearchAnexosItem {
   status: String;
   esquema: String;
   editar: boolean = false;
-  constructor() {}
+  idFicheroFirma: string;
+  constructor() { }
 }
