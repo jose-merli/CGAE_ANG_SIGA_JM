7d9a46057d82d9c30bcdc42599c6c8256c647078
Jesus Colchon <jcolchon@deloitte.es>
2019-03-27 17:41:31 +0100

    jcolchon incidencias censo

---
+++ src/app/features/censo/modificacion-datos/modificacion-datos.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { OldSigaServices } from '../../../_services/oldSiga.service'
+
+@Component({
+  selector: 'app-modificacion-datos',
+  templateUrl: './modificacion-datos.component.html',
+  styleUrls: ['./modificacion-datos.component.scss']
+})
+export class ModificacionDatosComponent {
+
+  url;
+
+  constructor(public sigaServices: OldSigaServices) {
+    this.url = sigaServices.getOldSigaUrl("modificacionDatos");
+  }
+
+  ngOnInit() {
+  }
+
+}
