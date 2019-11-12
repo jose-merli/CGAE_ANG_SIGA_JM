abfd63bc918a3279a7e02a6b9c4a10fa1b8148c9
Pablo Kämmerer <pkammerer@deloitte.es>
2019-11-12 16:32:30 +0100

    [pkammerer] => checkbox control

--- src/app/features/sjcs/guardia/busqueda-guardias/buscador-guardia/gestion-guardia/datos-calendarios-guardias/datos-calendarios-guardias.component.ts
+++ src/app/features/sjcs/guardia/busqueda-guardias/buscador-guardia/gestion-guardia/datos-calendarios-guardias/datos-calendarios-guardias.component.ts
@@ -9,43 +9,133 @@ export class DatosCalendariosGuardiasComponent implements OnInit {
 
   openFicha = false;
   modoEdicion = false
-  constructor() { }
+  selectLaborables = false;
+  selectFestividades = false;
 
-  semana = [
+  laborables = [
     {
       label: "L",
-
+      value: false
     },
     {
       label: "M",
+      value: false
 
     },
     {
       label: "X",
+      value: false
 
     },
     {
       label: "J",
+      value: false
 
     },
     {
       label: "V",
+      value: false
 
     },
     {
       label: "S",
+      value: false
 
     },
     {
       label: "D",
+      value: false
+
+    }
+  ]
+  festividades = [
+    {
+      label: "L",
+      value: false
+    },
+    {
+      label: "M",
+      value: false
+
+    },
+    {
+      label: "X",
+      value: false
+
+    },
+    {
+      label: "J",
+      value: false
+
+    },
+    {
+      label: "V",
+      value: false
+
+    },
+    {
+      label: "S",
+      value: false
+
+    },
+    {
+      label: "D",
+      value: false
 
     }
   ]
 
+  constructor() { }
+
   ngOnInit() {
     if (this.modoEdicion)
       this.openFicha = true;
+
+
   }
+
+  onChangeSeleccLaborables() {
+    if (!this.selectLaborables)
+      this.laborables.map(it => {
+        it.value = false
+        return it;
+      });
+    else
+      this.laborables.map(it => {
+        it.value = true
+        return it;
+      });
+  }
+  onChangeSeleccFestividades() {
+    if (!this.selectFestividades)
+      this.festividades.map(it => {
+        it.value = false
+        return it;
+      });
+    else
+      this.festividades.map(it => {
+        it.value = true
+        return it;
+      });
+  }
+
+  isAllLaborables() {
+    if (this.selectLaborables) {
+      this.laborables.forEach(it => {
+        if (!it.value)
+          this.selectLaborables = false;
+      })
+    }
+  }
+  isAllFestividades() {
+    if (this.selectFestividades) {
+      this.festividades.forEach(it => {
+        if (!it.value)
+          this.selectFestividades = false;
+      })
+    }
+  }
+
 
   abreCierraFicha() {
     this.openFicha = !this.openFicha;
