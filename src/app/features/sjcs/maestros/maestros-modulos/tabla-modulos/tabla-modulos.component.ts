a0fab7b7413f315d0c4202cd5833be438df9136b
Jesus Colchon <jcolchon@deloitte.es>
2019-12-10 11:33:02 +0100

    jcolchon incidencias maestros

--- src/app/features/sjcs/maestros/maestros-modulos/tabla-modulos/tabla-modulos.component.ts
+++ src/app/features/sjcs/maestros/maestros-modulos/tabla-modulos/tabla-modulos.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit, ViewChild, ChangeDetectorRef, Input, Output, EventEmitter, SimpleChanges } from '@angular/core';
+import {
+	Component,
+	OnInit,
+	ViewChild,
+	ChangeDetectorRef,
+	Input,
+	Output,
+	EventEmitter,
+	SimpleChanges
+} from '@angular/core';
 import { SigaServices } from '../../../../../_services/siga.service';
 import { TranslateService } from '../../../../../commons/translate/translation.service';
 import { ModulosItem } from '../../../../../models/sjcs/ModulosItem';
@@ -11,259 +20,256 @@ import { Router } from '../../../../../../../node_modules/@angular/router';
 import { SortEvent } from '../../../../../../../node_modules/primeng/api';
 
 @Component({
-  selector: 'app-tabla-modulos',
-  templateUrl: './tabla-modulos.component.html',
-  styleUrls: ['./tabla-modulos.component.scss']
+	selector: 'app-tabla-modulos',
+	templateUrl: './tabla-modulos.component.html',
+	styleUrls: [ './tabla-modulos.component.scss' ]
 })
 export class TablaModulosComponent implements OnInit {
+	rowsPerPage: any = [];
+	cols;
+	colsPartidoJudicial;
+	msgs;
+	buscadores = [];
+	selectedItem: number = 10;
+	selectAll;
+	selectedDatos = [];
+	numSelected = 0;
+	selectMultiple: boolean = false;
+	seleccion: boolean = false;
+	historico: boolean = false;
 
+	message;
+	permisos: boolean = false;
 
-  rowsPerPage: any = [];
-  cols;
-  colsPartidoJudicial;
-  msgs;
-  buscadores = [];
-  selectedItem: number = 10;
-  selectAll;
-  selectedDatos = [];
-  numSelected = 0;
-  selectMultiple: boolean = false;
-  seleccion: boolean = false;
-  historico: boolean = false;
+	initDatos;
+	nuevo: boolean = false;
+	progressSpinner: boolean = false;
 
-  message;
-  permisos: boolean = false;
+	//Resultados de la busqueda
+	@Input() datos;
+	//Combo partidos judiciales
+	@Input() comboPJ;
 
-  initDatos;
-  nuevo: boolean = false;
-  progressSpinner: boolean = false;
+	@Output() searchModulos = new EventEmitter<boolean>();
 
-  //Resultados de la busqueda
-  @Input() datos;
-  //Combo partidos judiciales
-  @Input() comboPJ;
+	@ViewChild('tabla') tabla;
 
-  @Output() searchModulos = new EventEmitter<boolean>();
+	constructor(
+		private translateService: TranslateService,
+		private changeDetectorRef: ChangeDetectorRef,
+		private router: Router,
+		private sigaServices: SigaServices,
+		private persistenceService: PersistenceService,
+		private confirmationService: ConfirmationService
+	) {}
 
-  @ViewChild("tabla") tabla;
+	ngOnInit() {
+		this.getCols();
+		this.initDatos = JSON.parse(JSON.stringify(this.datos));
+		if (this.persistenceService.getPermisos()) {
+			this.permisos = true;
+		} else {
+			this.permisos = false;
+		}
+	}
 
-  constructor(private translateService: TranslateService,
-    private changeDetectorRef: ChangeDetectorRef,
-    private router: Router,
-    private sigaServices: SigaServices,
-    private persistenceService: PersistenceService,
-    private confirmationService: ConfirmationService
-  ) { }
+	ngOnChanges(changes: SimpleChanges) {
+		this.datos.forEach((element) => {
+			element.importe = +element.importe;
+		});
+	}
 
-  ngOnInit() {
-    this.getCols();
-    this.initDatos = JSON.parse(JSON.stringify((this.datos)));
-    if (this.persistenceService.getPermisos()) {
-      this.permisos = true;
-    } else {
-      this.permisos = false;
-    }
-  }
+	customSort(event: SortEvent) {
+		event.data.sort((data1, data2) => {
+			let value1 = data1[event.field];
+			let value2 = data2[event.field];
+			let result = null;
 
-  ngOnChanges(changes: SimpleChanges) {
-    this.datos.forEach(element => {
-      element.importe = +element.importe;
-    });
-  }
+			if (value1 == null && value2 != null) result = -1;
+			else if (value1 != null && value2 == null) result = 1;
+			else if (value1 == null && value2 == null) result = 0;
+			else if (typeof value1 === 'string' && typeof value2 === 'string') result = value1.localeCompare(value2);
+			else result = value1 < value2 ? -1 : value1 > value2 ? 1 : 0;
 
-  customSort(event: SortEvent) {
-    event.data.sort((data1, data2) => {
-      let value1 = data1[event.field];
-      let value2 = data2[event.field];
-      let result = null;
+			return event.order * result;
+		});
+	}
 
-      if (value1 == null && value2 != null)
-        result = -1;
-      else if (value1 != null && value2 == null)
-        result = 1;
-      else if (value1 == null && value2 == null)
-        result = 0;
-      else if (typeof value1 === 'string' && typeof value2 === 'string')
-        result = value1.localeCompare(value2);
-      else
-        result = (value1 < value2) ? -1 : (value1 > value2) ? 1 : 0;
+	seleccionaFila(evento) {
+		if (!this.selectAll && !this.selectMultiple) {
+			this.persistenceService.setHistorico(this.historico);
+			this.persistenceService.setDatos(this.selectedDatos[0]);
+			this.router.navigate([ '/gestionModulos' ], {
+				queryParams: { idProcedimiento: this.selectedDatos[0].idProcedimiento }
+			});
+		} else {
+			if (evento.data.fechabaja == undefined && this.historico == true) {
+				this.selectedDatos.pop();
+			}
+		}
+	}
 
-      return (event.order * result);
-    });
-  }
+	confirmDelete(selectedDatos) {
+		let mess = this.translateService.instant('messages.deleteConfirmation');
+		let icon = 'fa fa-edit';
+		this.confirmationService.confirm({
+			message: mess,
+			icon: icon,
+			accept: () => {
+				this.delete(selectedDatos);
+			},
+			reject: () => {
+				this.msgs = [
+					{
+						severity: 'info',
+						summary: 'Cancelar',
+						detail: this.translateService.instant('general.message.accion.cancelada')
+					}
+				];
+			}
+		});
+	}
 
-  seleccionaFila(evento) {
-    if (!this.selectAll && !this.selectMultiple) {
-      this.persistenceService.setHistorico(this.historico);
-      this.persistenceService.setDatos(this.selectedDatos[0]);
-      this.router.navigate(["/gestionModulos"], { queryParams: { idProcedimiento: this.selectedDatos[0].idProcedimiento } });
-    } else {
-      if (evento.data.fechabaja == undefined && this.historico == true) {
-        this.selectedDatos.pop();
-      }
-    }
+	delete(selectedDatos) {
+		let ModulosDelete = new ModulosObject();
+		ModulosDelete.modulosItem = selectedDatos;
+		this.sigaServices.post('modulosybasesdecompensacion_deleteModulos', ModulosDelete).subscribe(
+			(data) => {
+				this.selectedDatos = [];
+				if (this.historico) {
+					this.searchModulos.emit(true);
+				} else {
+					this.searchModulos.emit(false);
+				}
+				this.showMessage(
+					'success',
+					this.translateService.instant('general.message.correct'),
+					this.translateService.instant('general.message.accion.realizada')
+				);
+				this.progressSpinner = false;
+			},
+			(err) => {
+				if (err != undefined && JSON.parse(err.error).error.description != '') {
+					this.showMessage(
+						'error',
+						this.translateService.instant('general.message.incorrect'),
+						this.translateService.instant(JSON.parse(err.error).error.description)
+					);
+				} else {
+					this.showMessage(
+						'error',
+						this.translateService.instant('general.message.incorrect'),
+						this.translateService.instant('general.message.error.realiza.accion')
+					);
+				}
+				this.progressSpinner = false;
+			},
+			() => {
+				this.progressSpinner = false;
+				this.historico = false;
+				this.selectAll = false;
+			}
+		);
+	}
 
-  }
+	onChangeSelectAll() {
+		if (this.permisos) {
+			if (this.selectAll === true) {
+				this.selectMultiple = false;
+				this.selectedDatos = this.datos;
+				this.numSelected = this.datos.length;
+				if (this.historico) {
+					this.selectedDatos = this.datos.filter(
+						(dato) => dato.fechabaja != undefined && dato.fechabaja != null
+					);
+				} else {
+					this.selectedDatos = this.datos;
+				}
+			} else {
+				this.selectedDatos = [];
+				this.numSelected = 0;
+			}
+		}
+	}
 
-  confirmDelete(selectedDatos) {
-    let mess = this.translateService.instant(
-      "messages.deleteConfirmation"
-    );
-    let icon = "fa fa-edit";
-    this.confirmationService.confirm({
-      message: mess,
-      icon: icon,
-      accept: () => {
-        this.delete(selectedDatos)
-      },
-      reject: () => {
-        this.msgs = [
-          {
-            severity: "info",
-            summary: "Cancelar",
-            detail: this.translateService.instant(
-              "general.message.accion.cancelada"
-            )
-          }
-        ];
-      }
-    });
-  }
+	searchModulo() {
+		this.historico = !this.historico;
+		this.searchModulos.emit(this.historico);
+	}
 
+	setItalic(dato) {
+		if (dato.fechabaja == null) return false;
+		else return true;
+	}
 
-  delete(selectedDatos) {
-    let ModulosDelete = new ModulosObject();
-    ModulosDelete.modulosItem = this.selectedDatos
-    this.sigaServices.post("modulosybasesdecompensacion_deleteModulos", ModulosDelete).subscribe(
-      data => {
-        this.selectedDatos = [];
-        if (this.historico) {
-          this.searchModulos.emit(true);
-        } else {
-          this.searchModulos.emit(false);
-        }
-        this.showMessage("success", this.translateService.instant("general.message.correct"), this.translateService.instant("general.message.accion.realizada"));
-        this.progressSpinner = false;
-      },
-      err => {
-        if (err != undefined && JSON.parse(err.error).error.description != "") {
-          this.showMessage("error", this.translateService.instant("general.message.incorrect"), this.translateService.instant(JSON.parse(err.error).error.description));
-        } else {
-          this.showMessage("error", this.translateService.instant("general.message.incorrect"), this.translateService.instant("general.message.error.realiza.accion"));
-        }
-        this.progressSpinner = false;
-      },
-      () => {
-        this.progressSpinner = false;
-        this.historico = false;
-        this.selectAll = false;
-      }
-    );
-  }
+	getCols() {
+		this.cols = [
+			{ field: 'codigo', header: 'general.boton.code', width: '12%' },
+			{ field: 'nombre', header: 'administracion.parametrosGenerales.literal.nombre', width: '42%' },
+			{ field: 'fechadesdevigor', header: 'facturacion.seriesFacturacion.literal.fInicio', width: '12%' },
+			{ field: 'fechahastavigor', header: 'censo.consultaDatos.literal.fechaFin', width: '12%' },
+			{ field: 'importe', header: 'formacion.fichaCurso.tarjetaPrecios.importe', width: '12%' }
+		];
+		this.cols.forEach((it) => this.buscadores.push(''));
 
-  onChangeSelectAll() {
-    if (this.permisos) {
-      if (this.selectAll === true) {
-        this.selectMultiple = false;
-        this.selectedDatos = this.datos;
-        this.numSelected = this.datos.length;
-        if (this.historico) {
-          this.selectedDatos = this.datos.filter(dato => dato.fechabaja != undefined && dato.fechabaja != null);
-        } else {
-          this.selectedDatos = this.datos;
-        }
-      } else {
-        this.selectedDatos = [];
-        this.numSelected = 0;
-      }
+		this.rowsPerPage = [
+			{
+				label: 10,
+				value: 10
+			},
+			{
+				label: 20,
+				value: 20
+			},
+			{
+				label: 30,
+				value: 30
+			},
+			{
+				label: 40,
+				value: 40
+			}
+		];
+	}
 
-    }
-  }
+	onChangeRowsPerPages(event) {
+		this.selectedItem = event.value;
+		this.changeDetectorRef.detectChanges();
+		this.tabla.reset();
+	}
 
-  searchModulo() {
-    this.historico = !this.historico;
-    this.searchModulos.emit(this.historico);
+	isSelectMultiple() {
+		if (this.permisos) {
+			this.selectAll = false;
+			this.selectMultiple = !this.selectMultiple;
+			if (!this.selectMultiple) {
+				this.selectedDatos = [];
+				this.numSelected = 0;
+			} else {
+				// this.pressNew = false;
+				this.selectAll = false;
+				this.selectedDatos = [];
+				this.numSelected = 0;
+			}
+		}
+		// this.volver();
+	}
 
-  }
+	actualizaSeleccionados(selectedDatos) {
+		this.numSelected = selectedDatos.length;
+		this.seleccion = false;
+	}
 
-  setItalic(dato) {
-    if (dato.fechabaja == null) return false;
-    else return true;
-  }
+	showMessage(severity, summary, msg) {
+		this.msgs = [];
+		this.msgs.push({
+			severity: severity,
+			summary: summary,
+			detail: msg
+		});
+	}
 
-  getCols() {
-
-    this.cols = [
-      { field: "codigo", header: "general.boton.code", width: "12%" },
-      { field: "nombre", header: "administracion.parametrosGenerales.literal.nombre", width: "42%" },
-      { field: "fechadesdevigor", header: "facturacion.seriesFacturacion.literal.fInicio", width: "12%" },
-      { field: "fechahastavigor", header: "censo.consultaDatos.literal.fechaFin", width: "12%" },
-      { field: "importe", header: "formacion.fichaCurso.tarjetaPrecios.importe", width: "12%" }
-
-    ];
-    this.cols.forEach(it => this.buscadores.push(""));
-
-    this.rowsPerPage = [
-      {
-        label: 10,
-        value: 10
-      },
-      {
-        label: 20,
-        value: 20
-      },
-      {
-        label: 30,
-        value: 30
-      },
-      {
-        label: 40,
-        value: 40
-      }
-    ];
-  }
-
-  onChangeRowsPerPages(event) {
-    this.selectedItem = event.value;
-    this.changeDetectorRef.detectChanges();
-    this.tabla.reset();
-  }
-
-  isSelectMultiple() {
-    if (this.permisos) {
-      this.selectAll = false;
-      this.selectMultiple = !this.selectMultiple;
-      if (!this.selectMultiple) {
-        this.selectedDatos = [];
-        this.numSelected = 0;
-      } else {
-        // this.pressNew = false;
-        this.selectAll = false;
-        this.selectedDatos = [];
-        this.numSelected = 0;
-      }
-    }
-    // this.volver();
-  }
-
-
-  actualizaSeleccionados(selectedDatos) {
-    this.numSelected = selectedDatos.length;
-    this.seleccion = false;
-  }
-
-  showMessage(severity, summary, msg) {
-    this.msgs = [];
-    this.msgs.push({
-      severity: severity,
-      summary: summary,
-      detail: msg
-    });
-  }
-
-  clear() {
-    this.msgs = [];
-  }
-
-
+	clear() {
+		this.msgs = [];
+	}
 }
