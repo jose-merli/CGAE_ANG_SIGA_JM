65c45e8d03d8d4874eef93f0a1a19853d3ae4575
Fredy Jerbi Echtati <fjerbi@deloitte.es>
2019-12-10 11:43:09 +0100

    [fjerbi] Agregado componente general busqueda de Asuntos

---
+++ src/app/models/sjcs/AsistenciasItem.ts
@@ -0,0 +1,30 @@
+
+export class AsistenciasItem {
+
+    anioRegistro: string;
+    nombre: string;
+    apellidos: string;
+    numero: string;
+    idInstitucion: string;
+    fechadesde: Date;
+    fechahasta: Date;
+    idTurno: string;
+    idGuardia: string;
+    nif: string;
+    nig: string;
+    idTipoEjg: string;
+    idTipoEjgColegio: string;
+    idEstadoEjg: string;
+    idTipoDesignacion: string;
+    anioProcedimiento: string;
+    numProcedimiento: string;
+    numProcedimientoRegistro: string;
+    idJuzgado: string;
+    nDiligencia: string;
+    nAsunto: string;
+    idCentroDetencion: string;
+
+    constructor() { }
+}
+
+
