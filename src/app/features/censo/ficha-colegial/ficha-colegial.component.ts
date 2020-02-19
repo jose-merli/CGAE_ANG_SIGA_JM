4f4502eaacd45105f9f62aa69979df6da8b99b0f
María José <mtubio@deloitte.es>
2020-02-19 13:32:32 +0100

    [mtubio] Ficha colegial, restablecer nuevo no colegiados en tarjeta datos generales y restablecer fecha de nacimiento, deshabilitar combo en datos bancarios si es historico, quitar el color azul del correo electronico en la tabla de busqueda de colegiados

--- src/app/features/censo/ficha-colegial/ficha-colegial.component.ts
+++ src/app/features/censo/ficha-colegial/ficha-colegial.component.ts
@@ -2071,6 +2071,10 @@ export class FichaColegialComponent implements OnInit {
   restablecerGenerales() {
     if (this.esNewColegiado) {
       this.generalBody = new FichaColegialGeneralesItem();
+      this.generalBody = JSON.parse(JSON.stringify(this.colegialesBody));
+      if( this.generalBody.nif != undefined &&  this.generalBody.nif != "" &&  this.generalBody != null){
+        this.compruebaDNI();
+      }
       this.etiquetasPersonaJuridicaSelecionados = this.generalBody.etiquetas;
       this.obtenerEtiquetasPersonaJuridicaConcreta();
       this.stringAComisiones();
@@ -2078,6 +2082,10 @@ export class FichaColegialComponent implements OnInit {
     } else {
       this.cargarImagen(this.idPersona);
       this.generalBody = JSON.parse(JSON.stringify(this.checkGeneralBody));
+      if(this.generalBody.fechaNacimiento != undefined && this.generalBody.fechaNacimiento != null){
+        this.fechaNacimiento = this.arreglarFecha(this.generalBody.fechaNacimiento);
+        this.calcularEdad(this.generalBody.fechaNacimiento);
+      }
       this.etiquetasPersonaJuridicaSelecionados = this.generalBody.etiquetas;
       this.obtenerEtiquetasPersonaJuridicaConcreta();
       this.stringAComisiones();
@@ -5743,6 +5751,7 @@ export class FichaColegialComponent implements OnInit {
   borrarFechaNacimiento() {
     this.fechaNacimiento = null;
     this.generalBody.fechaNacimientoDate = undefined;
+    this.generalBody.fechaNacimiento = undefined;
     this.fechaNacimientoSelected = true;
     this.edadCalculada = 0;
     this.calendarFechaNacimiento.onClearButtonClick("");
