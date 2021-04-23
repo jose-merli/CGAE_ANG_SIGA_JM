423e1929c321913656e115f2fd78623006e29025
Guillermo Rejon <grejon@deloitte.es>
2021-04-23 14:30:51 +0200

    Declaracion ficha letrados.

---
+++ src/app/features/sjcs/oficio/designaciones/ficha-designaciones/detalle-tarjeta-letrados-designa/ficha-cambio-letrado/letrado-saliente/letrado-saliente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-letrado-saliente',
+  templateUrl: './letrado-saliente.component.html',
+  styleUrls: ['./letrado-saliente.component.scss']
+})
+export class LetradoSalienteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
