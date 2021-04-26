46380c0a2d8e310e5a138a1d60637524730ab2c5
Miguel Ángel Daza Laprida <mdaza@deloitte.es>
2021-04-26 14:15:44 +0200

    Tarjeta Datos Facturación de la Ficha de Actuación

--- src/app/features/sjcs/oficio/designaciones/ficha-designaciones/detalle-tarjeta-actuaciones-designa/ficha-actuacion/tarjeta-datos-fact-ficha-act/tarjeta-datos-fact-ficha-act.component.ts
+++ src/app/features/sjcs/oficio/designaciones/ficha-designaciones/detalle-tarjeta-actuaciones-designa/ficha-actuacion/tarjeta-datos-fact-ficha-act/tarjeta-datos-fact-ficha-act.component.ts
@@ -1,15 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { Message } from 'primeng/components/common/api';
 import { SigaServices } from '../../../../../../../../_services/siga.service';
 import { CommonsService } from '../../../../../../../../_services/commons.service';
 import { Actuacion } from '../../detalle-tarjeta-actuaciones-designa.component';
+import { DesignaItem } from '../../../../../../../../models/sjcs/DesignaItem';
+import { TranslateService } from '../../../../../../../../commons/translate/translation.service';
 
 @Component({
   selector: 'app-tarjeta-datos-fact-ficha-act',
   templateUrl: './tarjeta-datos-fact-ficha-act.component.html',
   styleUrls: ['./tarjeta-datos-fact-ficha-act.component.scss']
 })
-export class TarjetaDatosFactFichaActComponent implements OnInit {
+export class TarjetaDatosFactFichaActComponent implements OnInit, OnDestroy {
 
   selector =
     {
@@ -21,13 +23,14 @@ export class TarjetaDatosFactFichaActComponent implements OnInit {
   msgs: Message[] = [];
   @Input() isAnulada: boolean;
   @Input() actuacionDesigna: Actuacion;
+
+  @Output() changeDataEvent = new EventEmitter<any>();
   progressSpinner: boolean = false;
 
-  constructor(private sigaServices: SigaServices, private commonsService: CommonsService) { }
+  constructor(private sigaServices: SigaServices, private commonsService: CommonsService, private translateService: TranslateService) { }
 
   ngOnInit() {
     this.getComboPartidaPresupuestaria();
-    this.selector.value = this.actuacionDesigna.actuacion.idFacturacion;
   }
 
   getComboPartidaPresupuestaria() {
@@ -44,10 +47,82 @@ export class TarjetaDatosFactFichaActComponent implements OnInit {
         console.log(err);
       }, () => {
         this.progressSpinner = false;
+        this.getIdPartidaPresupuestaria();
       }
     );
   }
 
+  getIdPartidaPresupuestaria() {
+
+    this.progressSpinner = true;
+
+    let factAct = new DesignaItem();
+    factAct.idTurno = Number(this.actuacionDesigna.actuacion.idTurno);
+    factAct.ano = Number(this.actuacionDesigna.actuacion.anio);
+    factAct.numero = this.actuacionDesigna.designaItem.numero;
+
+    this.sigaServices.post("designaciones_getDatosFacturacion", factAct).subscribe(
+      n => {
+        let resp = JSON.parse(n.body).combooItems;
+        if (resp.length > 0) {
+          this.selector.value = resp[0].value;
+          this.changeDataEvent.emit({ tarjeta: 'sjcsDesigActuaOfiDatFac', partida: resp.label });
+          sessionStorage.setItem("datosIniActuDesignaDatosFact", JSON.stringify(resp[0]));
+        }
+      },
+      err => {
+        this.progressSpinner = false;
+        console.log(err);
+      }, () => {
+        this.progressSpinner = false;
+      }
+    );
+  }
+
+  guardar() {
+
+    this.progressSpinner = true;
+
+    let factAct = new DesignaItem();
+    factAct.idTurno = Number(this.actuacionDesigna.actuacion.idTurno);
+    factAct.ano = Number(this.actuacionDesigna.actuacion.anio);
+    factAct.numero = this.actuacionDesigna.designaItem.numero;
+    factAct.idPartidaPresupuestaria = Number(this.selector.value);
+
+    this.sigaServices.post("designaciones_updateDatosFacturacion", factAct).subscribe(
+      n => {
+        this.progressSpinner = false;
+        const resp = JSON.parse(n.body);
+
+        if (resp.error != null && resp.error.code == 200) {
+          this.showMsg('success', this.translateService.instant('general.message.correct'), this.translateService.instant('general.message.accion.realizada'));
+          this.changeDataEvent.emit({ tarjeta: 'sjcsDesigActuaOfiDatFac', partida: this.selector.opciones.find(el => el.value == this.selector.value).label });
+          sessionStorage.setItem("datosIniActuDesignaDatosFact", JSON.stringify({ label: this.selector.opciones.find(el => el.value == this.selector.value).label, value: this.selector.value }));
+        }
+
+        if (resp.error != null && resp.error.descripcion != null) {
+          this.showMsg('error', 'Error', resp.error.descripcion);
+        }
+
+      },
+      err => {
+        this.progressSpinner = false;
+        console.log(err);
+      }, () => {
+        this.progressSpinner = false;
+      }
+    );
+  }
+
+  restablecer() {
+
+    if (sessionStorage.getItem("datosIniActuDesignaDatosFact")) {
+      let valorIni = JSON.parse(sessionStorage.getItem("datosIniActuDesignaDatosFact"));
+      this.selector.value = valorIni.value;
+      this.changeDataEvent.emit({ tarjeta: 'sjcsDesigActuaOfiDatFac', partida: valorIni.label });
+      this.showMsg('success', this.translateService.instant('general.message.correct'), this.translateService.instant('general.message.accion.realizada'));
+    }
+  }
 
   showMsg(severity, summary, detail) {
     this.msgs = [];
@@ -60,6 +135,10 @@ export class TarjetaDatosFactFichaActComponent implements OnInit {
 
   clear() {
     this.msgs = [];
+  }
+
+  ngOnDestroy(): void {
+    sessionStorage.removeItem("datosIniActuDesignaDatosFact");
   }
 
 }
