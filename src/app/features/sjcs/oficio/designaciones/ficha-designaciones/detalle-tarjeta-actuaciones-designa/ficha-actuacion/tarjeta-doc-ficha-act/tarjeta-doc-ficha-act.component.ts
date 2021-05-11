610c7010732b311a040edcb11eef7f3aa2269319
Miguel Ángel Daza Laprida <mdaza@deloitte.es>
2021-05-11 15:00:30 +0200

    Se corrige algún fallo de la ficha actuación, se hace que no se puedan edirtar actuaciones si esta está validada y no es colegio y se muestre en modo lectura

--- src/app/features/sjcs/oficio/designaciones/ficha-designaciones/detalle-tarjeta-actuaciones-designa/ficha-actuacion/tarjeta-doc-ficha-act/tarjeta-doc-ficha-act.component.ts
+++ src/app/features/sjcs/oficio/designaciones/ficha-designaciones/detalle-tarjeta-actuaciones-designa/ficha-actuacion/tarjeta-doc-ficha-act/tarjeta-doc-ficha-act.component.ts
@@ -25,7 +25,10 @@ export class TarjetaDocFichaActComponent implements OnInit, OnChanges {
   @Input() usuarioLogado;
   @Input() isColegiado;
   @Input() isAnulada;
+  @Input() modoLectura: boolean;
+
   @Output() buscarDocumentosEvent = new EventEmitter<any>();
+  
   documentos2: Documento[];
   cols: Col[] = [
     {
