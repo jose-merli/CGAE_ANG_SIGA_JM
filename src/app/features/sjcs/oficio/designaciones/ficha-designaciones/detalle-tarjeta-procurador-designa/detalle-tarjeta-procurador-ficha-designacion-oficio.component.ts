d7144100ffd072643a97393cca8826de9b2d7443
Guillermo Rejon <grejon@deloitte.es>
2021-07-02 09:14:44 +0200

    Corregida limitacion en campo de numero de designacion de la tarjeta de procurador de ficha pre-designacion. Correccion del campo "Numero de colegiado" en la pantalla de busqueda de procuradores. Antes no dejaba introducir numeros de colegiado con menos de 3 digitos. Se ha implmentado el servicio de guardado de procurador y el añadido de nuevo procurador. Correccion de la cabecera de la tarjeta de procurador en designacion. Implementado el que se abra y se mueva el scroll al seleccionar un procurador nuevo. Implmentacion de columnas indpendientes en la tarjeta de procurador. Implmentacion de restriccion de fecha solicitud de renuncia. Añadidas atributos de clave primaria de designacion al objeyo "ProcuradorItem".

--- src/app/features/sjcs/oficio/designaciones/ficha-designaciones/detalle-tarjeta-procurador-designa/detalle-tarjeta-procurador-ficha-designacion-oficio.component.ts
+++ src/app/features/sjcs/oficio/designaciones/ficha-designaciones/detalle-tarjeta-procurador-designa/detalle-tarjeta-procurador-ficha-designacion-oficio.component.ts
@@ -8,6 +8,8 @@ import { ProcuradorItem } from '../../../../../../models/sjcs/ProcuradorItem';
 import { SigaServices } from '../../../../../../_services/siga.service';
 import { TranslateService } from '../../../../../../commons/translate';
 import { CommonsService } from '../../../../../../_services/commons.service';
+import { Router } from '@angular/router';
+import { DesignaItem } from '../../../../../../models/sjcs/DesignaItem';
 
 @Component({
   selector: 'app-detalle-tarjeta-procurador-ficha-designacion-oficio',
@@ -22,6 +24,7 @@ export class DetalleTarjetaProcuradorFichaDesignacionOficioComponent implements 
   @Input() rowGroupsAux: Row[];
   @Input() seleccionarTodo = false;
   @Input() totalRegistros = 0;
+  @Input() procuradores;
   numCabeceras = 0;
   numColumnas = 0;
   numperPage = 10;
@@ -36,7 +39,7 @@ export class DetalleTarjetaProcuradorFichaDesignacionOficioComponent implements 
   showModal2: boolean = false;
   showModal3: boolean = false;
   isLetrado: boolean = false;
-  isDisabled: boolean = false;
+  nuevoProcurador: boolean = false;
 
   comboMotivo = [
     { label: "Vacaciones", value: "V" },
@@ -45,20 +48,22 @@ export class DetalleTarjetaProcuradorFichaDesignacionOficioComponent implements 
     { label: "Suspensión por sanción", value: "S" }
   ];
   
-  @Output() modDatos = new EventEmitter<any>();
   @Output() mostrar = new EventEmitter<any>();
   @Output() restablecer = new EventEmitter<any>();
   @Output() anySelected = new EventEmitter<any>();
-  @Output() nuevo = new EventEmitter<any>();
+
   datosProcurador: ProcuradorItem = new ProcuradorItem();
   progressSpinner: boolean = false;
-  disabledSave: boolean = true;
+  //disabledSave: boolean = true;
+
+  fechaDesigna: Date = new Date();
 
   constructor(
     private renderer: Renderer2,
     private pipe: DatePipe, private sigaServices: SigaServices,
     private translateService: TranslateService,
-    private commonServices: CommonsService) { 
+    private commonsService: CommonsService,
+    private router: Router) { 
     this.renderer.listen('window', 'click', (event: { target: HTMLInputElement; }) => {
     for (let i = 0; i < this.table.nativeElement.children.length; i++) {
 
@@ -70,19 +75,48 @@ export class DetalleTarjetaProcuradorFichaDesignacionOficioComponent implements 
   });}
 
   ngOnInit() {
-    if(sessionStorage.getItem("datosProcurador") != null){
+    //En el caso que se traiga un procurador desde la pantalla de busqueda de procuradores
+    if (sessionStorage.getItem("datosProcurador")) {
+      //Para mover el scroll a la tajeta si se selecciona un procurador nuevo
+      let proc = document.getElementById("procurador");
+      if (proc) {
+        setTimeout(() => {
+        proc.scrollIntoView();
+        proc = null;
+      }, 15);
+      }
+
       let data =JSON.parse(sessionStorage.getItem("datosProcurador"));
+      sessionStorage.removeItem("datosProcurador");
+
+      this.nuevoProcurador = true;
 
       this.datosProcurador=data[0];
     }
+    //Reasignamos las fechas al traerse del back en formato string
+    //No se realiza directamente ya que la conversion de new Date con strings se realiza desde MM/DD/YYYY y se nos devuelve DD/MM/YYY desde el back
+    else{
+      //Fecha designacion
+      var dateString = this.rowGroups[0].cells[0].value; 
 
-    if(sessionStorage.getItem("compruebaProcurador")){
-      this.disabledSave = false;
-      sessionStorage.removeItem("compruebaProcurador");
-    }
+      var dateParts = dateString.split("/");
 
-    if(sessionStorage.getItem("nuevoProcurador")){
-      this.showModal = true;
+      // month is 0-based, that's why we need dataParts[1] - 1
+      this.fechaDesigna = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
+      this.rowGroups[0].cells[0].value = this.fechaDesigna;
+
+
+      //Fecha solicitud de renuncia
+      if(this.rowGroups[0].cells[6].value){
+      var dateString = this.rowGroups[0].cells[6].value; 
+
+      var dateParts = dateString.split("/");
+
+      // month is 0-based, that's why we need dataParts[1] - 1
+      this.rowGroups[0].cells[6].value = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
+      }
+
+      this.datosProcurador = this.procuradores[0];
     }
 
     this.numCabeceras = this.cabeceras.length;
@@ -96,6 +130,17 @@ export class DetalleTarjetaProcuradorFichaDesignacionOficioComponent implements 
 
     sessionStorage.removeItem("nuevoProcurador");
   }
+
+  numberOnly(event): boolean {
+		const charCode = (event.which) ? event.which : event.keyCode;
+		if (charCode >= 48 && charCode <= 57) {
+			return true;
+		}
+		else {
+			return false;
+
+		}
+	}
 
   mostrarDatos(){
     this.mostrar.emit();
@@ -116,8 +161,13 @@ export class DetalleTarjetaProcuradorFichaDesignacionOficioComponent implements 
     this.numperPage = perPage;
   }
   fillFecha(event, cell) {
-    cell.value = this.pipe.transform(event, 'dd/MM/yyyy');
+    cell.value = event;
   }
+  fillFechaDesigna(event, cell) {
+    cell.value = event;
+    this.fechaDesigna = event;
+  }
+
   sortData(sort: Sort) {
     let data: Row[] = [];
     this.rowGroups = this.rowGroupsAux.filter((row) => {
@@ -199,10 +249,75 @@ export class DetalleTarjetaProcuradorFichaDesignacionOficioComponent implements 
       sessionStorage.setItem("rowGroupsInitProcurador", JSON.stringify(changes.rowGroups.currentValue));
     }
   }
+
+  disableGuardar(){
+    //Se revisa la primera fila que es la editable.
+    //Campos respectivamente: "Fecha designaicon", "Numero Designacion" y "Observaciones"
+    if(this.rowGroups[0].cells[0].value !=null 
+      //&& this.rowGroups[0].cells[1].value !=null 
+      && this.rowGroups[0].cells[5].value !=null){
+      return false;
+    }
+    else return true;
+  }
  
   checkGuardar(){
-    this.modDatos.emit(this.rowGroups);
-    this.totalRegistros = this.rowGroups.length;
+    if(this.disableGuardar()){
+      this.msgs = [{ severity: "error", summary: "Error", detail: this.translateService.instant('general.message.camposObligatorios') }];
+    }
+    else{
+      this.comprobarFechaProcurador();
+    }
+  }
+
+  guardarProc(){
+    this.progressSpinner = true;
+
+    let array = [];
+    let array2 = [];
+
+    this.rowGroups.forEach(element => {
+      element.cells.forEach(dato => {
+        array.push(dato.value);
+      });
+      array2.push(array);
+      array = [];
+    });
+
+    let procuradorPeticion : ProcuradorItem = new ProcuradorItem();
+
+    //Se asignan los datos traidos de la pantalla de busqueda.
+    procuradorPeticion.idInstitucion = this.datosProcurador.idInstitucion;
+    procuradorPeticion.idProcurador = this.datosProcurador.idProcurador;
+
+    //Se asignan los valores de la designa.
+    let designa = JSON.parse(sessionStorage.getItem("designaItemLink"))
+    procuradorPeticion.idTurno = designa.idTurno.toString();
+    procuradorPeticion.anio = designa.anio;
+    procuradorPeticion.numero = designa.numero.toString();
+
+    //Se asignan los valores asignados en la tabla.
+    procuradorPeticion.fechaDesigna = this.rowGroups[0].cells[0].value;
+    procuradorPeticion.numerodesignacion = this.rowGroups[0].cells[1].value;
+    procuradorPeticion.motivosRenuncia = this.rowGroups[0].cells[4].value;
+    procuradorPeticion.observaciones = this.rowGroups[0].cells[5].value;
+    procuradorPeticion.fecharenunciasolicita = this.rowGroups[0].cells[6].value;
+
+    //Para que el back sepa si se trata de la actualizacion de un procurador existente o la introducion de uno nuevo.
+    procuradorPeticion.numeroTotalProcuradores = this.rowGroups.length.toString(); 
+
+
+    this.sigaServices.post("designaciones_guardarProcurador", procuradorPeticion).subscribe(
+      data => {
+        this.showMessage("success", this.translateService.instant("general.message.correct"), this.translateService.instant("general.message.accion.realizada"));
+        this.progressSpinner = false;
+        this.mostrarDatos();
+      },
+      err => {
+        this.showMessage("error", this.translateService.instant("general.message.incorrect"), this.translateService.instant("general.message.error.realiza.accion"));
+        this.progressSpinner = false;
+      }
+    );
   }
 
   checkRestablecer(){
@@ -211,8 +326,8 @@ export class DetalleTarjetaProcuradorFichaDesignacionOficioComponent implements 
   }
 
   checkNuevo(){
-    this.nuevo.emit();
-    this.totalRegistros = this.rowGroups.length;
+    sessionStorage.setItem("nuevoProcurador", "true");
+		this.router.navigate(['/busquedaGeneral']);
   }
 
   cerrarModal(){
@@ -225,22 +340,25 @@ export class DetalleTarjetaProcuradorFichaDesignacionOficioComponent implements 
     this.datosProcurador.fechaDesigna = null;
   }
 
-  checkNuevoProcurador(){
-      this.progressSpinner = true;
-      console.log(this.datosProcurador);
-  
-      this.sigaServices.post("designaciones_nuevoProcurador", this.datosProcurador).subscribe(
-        data => {
-          let error = JSON.parse(data.body).error;
-          this.showMessage("info", this.translateService.instant("general.message.correct"), error.description);
-          this.progressSpinner = false;
-        },
-        err => {
-          let error = JSON.parse(err.body).error;
-          this.showMessage("info", this.translateService.instant("general.message.incorrect"), error.description);
-          this.progressSpinner = false;
-        }
-      );
+  styleObligatorio(evento) {
+    if (evento == undefined || evento == null || evento == "") {
+      return this.commonsService.styleObligatorio(evento);
+    }
+  }
+
+  guardarProcuradorEJG(proc) {
+    this.progressSpinner = true;
+
+    this.sigaServices.post("designaciones_guardarProcuradorEJG", proc).subscribe(
+      data => {
+        this.showMessage("success", this.translateService.instant("general.message.correct"), this.translateService.instant("general.message.accion.realizada"));
+        this.progressSpinner = false;
+      },
+      err => {
+        this.showMessage("error", this.translateService.instant("general.message.incorrect"), this.translateService.instant("general.message.error.realiza.accion"));
+        this.progressSpinner = false;
+      }
+    );
   }
 
   showMessage(severity, summary, msg) {
@@ -255,6 +373,109 @@ export class DetalleTarjetaProcuradorFichaDesignacionOficioComponent implements 
     clear() {
     this.msgs = [];
   }
+
+  // modDatos(event) {
+  //   let array = [];
+  //   let array2 = [];
+  //     event.forEach(element => {
+  //       element.cells.forEach(dato => {
+  //         array.push(dato.value);
+  //       });
+  //       array2.push(array);
+  //       array = [];
+  //     });
+  //     this.compruebaProcurador(array2[0]);
+    
+  // }
+
+  // compruebaProcurador(event) {
+  //   this.progressSpinner = true;
+
+    
+
+  //   this.sigaServices.post("designaciones_comprobarProcurador", procuradorPeticion).subscribe(
+  //     data => {
+
+  //       if (JSON.parse(data.body).procuradorItems[0] != undefined) {
+  //         this.showModal2 = true;
+  //       } else {
+  //         this.comprobarFechaProcurador();
+  //       }
+  //       this.progressSpinner = false;
+  //     },
+  //     err => {
+  //       this.showMessage("error", this.translateService.instant("general.message.incorrect"), this.translateService.instant("general.message.error.realiza.accion"));
+  //       this.progressSpinner = false;
+  //     }
+  //   );
+  // }
+
+  checkEJGDesignas(){
+    let designa = JSON.parse(sessionStorage.getItem("designaItemLink"))
+
+    this.sigaServices.post("gestionejg_getEjgDesigna", designa).subscribe(
+      n => {
+        let ejgDesignas = JSON.parse(n.body).ejgDesignaItems;
+        // if(ejgDesignas.length==0) this.isAssociated = false;
+        // else this.isAssociated = true;
+      }
+    );
+  }
+
+  comprobarFechaProcurador() {
+    this.progressSpinner = true;
+
+    let procuradorPeticion : ProcuradorItem = new ProcuradorItem();
+
+    //Se asignan los valores de la designa.
+    let designa = JSON.parse(sessionStorage.getItem("designaItemLink"))
+    procuradorPeticion.idTurno = designa.idTurno.toString();
+    procuradorPeticion.anio = designa.anio;
+    procuradorPeticion.numero = designa.numero.toString();
+
+    //Se asignan los valores asignados en la tabla.
+    procuradorPeticion.fechaDesigna = this.rowGroups[0].cells[0].value;
+
+    this.sigaServices.post("designaciones_comprobarFechaProcurador", procuradorPeticion).subscribe(
+      data => {
+
+        //Se comprueba si se ha devuelto algún procurador que tenga la misma fecha de designacion
+        if (!JSON.parse(data.body).procuradorItems.isEmpty()) {
+          this.showModal3 = true;
+        } else {
+          this.guardarProc();
+        }
+        this.progressSpinner = false;
+      },
+      err => {
+        this.showMessage("error", this.translateService.instant("general.message.incorrect"), this.translateService.instant("general.message.error.realiza.accion"));
+        this.progressSpinner = false;
+      }
+    );
+  }
+
+  actualizarProcurador() {
+    this.progressSpinner = true;
+    let array = [];
+      this.rowGroups[0].cells.forEach(dato => {
+        array.push(dato.value);
+      });
+    this.sigaServices.post("designaciones_actualizarProcurador", array).subscribe(
+      data => {
+        this.showMessage("success", this.translateService.instant("general.message.correct"), this.translateService.instant("general.message.accion.realizada"));
+        this.progressSpinner = false;
+      },
+      err => {
+        this.showMessage("error", this.translateService.instant("general.message.incorrect"), this.translateService.instant("general.message.error.realiza.accion"));
+        this.progressSpinner = false;
+      }
+    );
+  }
+
+  disableEdition(){
+    return this.rowGroups.length == 0;
+  }
+
 }
 function compare(a: string, b: number | string, isAsc: boolean) {
   return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
