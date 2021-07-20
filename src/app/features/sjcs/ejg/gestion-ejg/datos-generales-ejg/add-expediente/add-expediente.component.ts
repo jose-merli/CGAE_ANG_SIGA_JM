65038e095a2b426344b972e0e05736ceeb27cc35
Jose Luis Romero <jromeroprieto@deloitte.es>
2021-07-20 14:35:47 +0200

    [jromeroprieto] cambios para el servicio de solicitar eejg

--- src/app/features/sjcs/ejg/gestion-ejg/datos-generales-ejg/add-expediente/add-expediente.component.ts
+++ src/app/features/sjcs/ejg/gestion-ejg/datos-generales-ejg/add-expediente/add-expediente.component.ts
@@ -19,7 +19,7 @@ import { Router } from '@angular/router';
 
     constructor(public oldSigaServices: OldSigaServices, private sigaServices: SigaServices,
       private location: Location, private persistenceService: PersistenceService) {
-        this.url = oldSigaServices.getOldSigaUrl("url");
+        this.url = "google.es"//oldSigaServices.getOldSigaUrl("url");
       }
 
     ngOnInit() {
