adb4aa4ca253943e35099ba594d3e3bf4c7695bc
Jose Luis Romero <jromeroprieto@deloitte.es>
2021-08-30 11:23:53 +0200

    cambios para el expediente de insostenibilidad

---
+++ src/app/models/sjcs/ExpInsosItem.ts
@@ -0,0 +1,15 @@
+export class ExpInsosItem {
+
+    numero: string;
+    numEjg: string;
+    anioEJG: string;
+    idTipoEJG: string;
+    idInstitucion: string;
+    idProcedimiento: string;
+    idJuzgado: string;
+    idPretension: string;
+    idTurno: string;
+    nombre: string;
+
+    constructor() { }
+}
