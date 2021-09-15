7be72daceb5fd0f9b145a38673b8a9ed016d769f
Rubén Laborda <rlaborda@deloitte.es>
2021-09-15 11:47:21 +0200

    Añadido el filtro a las remesas de resultado

---
+++ src/app/features/sjcs/remesas-resultados/filtro-remesas-resultados/filtro-remesas-resultados.component.ts
@@ -0,0 +1,107 @@
+import { Component, OnInit, EventEmitter, HostListener, Input, ViewChild, Output } from '@angular/core';
+import { KEY_CODE } from '../../../censo/busqueda-personas-juridicas/busqueda-personas-juridicas.component';
+import { Router } from '@angular/router';
+import { TranslateService } from '../../../../commons/translate';
+import { SigaServices } from '../../../../_services/siga.service';
+import { CommonsService } from '../../../../_services/commons.service';
+import { PersistenceService } from '../../../../_services/persistence.service';
+import { RemesasResultadoItem } from '../../../../models/sjcs/RemesasResultadoItem';
+
+@Component({
+  selector: 'app-filtro-remesas-resultados',
+  templateUrl: './filtro-remesas-resultados.component.html',
+  styleUrls: ['./filtro-remesas-resultados.component.scss']
+})
+export class FiltroRemesasResultadosComponent implements OnInit {
+
+  showDatosGenerales: boolean = true;
+  msgs = [];
+
+  filtros: RemesasResultadoItem = new RemesasResultadoItem();
+  filtroAux: RemesasResultadoItem = new RemesasResultadoItem();
+  historico: boolean = false;
+
+
+  //@Input() permisoEscritura;
+  @Output() filtrosValues = new EventEmitter<RemesasResultadoItem>();
+
+  constructor(private router: Router, private translateService: TranslateService, private sigaServices: SigaServices,
+    private persistenceService: PersistenceService, private commonServices: CommonsService) { }
+
+  ngOnInit() {
+
+    if (this.persistenceService.getPermisos() != undefined) {
+      // this.permisoEscritura = this.persistenceService.getPermisos();
+    }
+
+    if (this.persistenceService.getFiltros() != undefined) {
+      this.filtros = this.persistenceService.getFiltros();
+      if (this.persistenceService.getHistorico() != undefined) {
+        this.historico = this.persistenceService.getHistorico();
+      }
+
+    } else {
+      this.filtros = new RemesasResultadoItem();
+    }
+
+  }
+
+  fillFechaRemesaDesde(event) {
+    if (event != null) {
+      this.filtros.fechaRemesaDesde = event;
+    }
+  }
+
+  fillFechaRemesaHasta(event) {
+    if (event != null) {
+      this.filtros.fechaRemesaHasta = event;
+    }
+  }
+
+  fillFechaCargaDesde(event) {
+    if (event != null) {
+      this.filtros.fechaCargaDesde = event;
+    }
+  }
+
+  fillFechaCardaHasta(event) {
+    if (event != null) {
+      this.filtros.fechaCargaHasta = event;
+    }
+  }
+
+  onHideDatosGenerales() {
+    this.showDatosGenerales = !this.showDatosGenerales;
+  }
+
+  search() {
+      this.filtrosValues.emit(this.filtros);
+  }
+
+  clearFilters() {
+    this.filtros = new RemesasResultadoItem();
+  }
+
+  clear() {
+    this.msgs = [];
+  }
+
+  showMessage(severity, summary, msg) {
+    this.msgs = [];
+    this.msgs.push({
+      severity: severity,
+      summary: summary,
+      detail: msg
+    });
+  }
+
+  //búsqueda con enter
+  @HostListener("document:keypress", ["$event"])
+  onKeyPress(event: KeyboardEvent) {
+    if (event.keyCode === KEY_CODE.ENTER) {
+      this.search();
+    }
+  }
+
+
+}
--- src/app/features/sjcs/remesas-resultados/remesas-resultados.component.ts
+++ src/app/features/sjcs/remesas-resultados/remesas-resultados.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 import { RemesasResultadoItem } from '../../../models/sjcs/RemesasResultadoItem';
 import { SigaServices } from '../../../_services/siga.service';
+import { FiltroRemesasResultadosComponent } from './filtro-remesas-resultados/filtro-remesas-resultados.component';
 
 
 @Component({
@@ -14,14 +15,37 @@ export class RemesasResultadosComponent implements OnInit {
   datos: RemesasResultadoItem = new RemesasResultadoItem();
   remesasResultadosItem: RemesasResultadoItem = new RemesasResultadoItem();
 
+  filtrosValues: RemesasResultadoItem = new RemesasResultadoItem();
+  @ViewChild(FiltroRemesasResultadosComponent) filtros;
+
   constructor(private sigaServices: SigaServices) { }
 
   ngOnInit() { }
 
+  getFiltrosValues(event) {
+    this.filtrosValues = JSON.parse(JSON.stringify(event));
+    this.convertArraysToStrings();
+    this.search();
+  }
+
+  convertArraysToStrings() {
+    const array = ['numRemesaPrefijo', 'numRemesaNumero', 'numRegistroPrefijo', 'numRegistroNumero', 'numRegistroSufijo', 'nombreFichero', 'fechaRemesaDesde', 'fechaRemesaHasta', 'fechaCargaDesde', 'fechaCargaHasta'];
+    if (this.filtrosValues != undefined) {
+      array.forEach(element => {
+        if (this.filtrosValues[element] != undefined && this.filtrosValues[element] != null && this.filtrosValues[element].length > 0) {
+          let aux = this.filtrosValues[element].toString();
+          this.filtrosValues[element] = aux;
+        }
+        if (this.filtrosValues[element] != undefined && this.filtrosValues[element] != null && this.filtrosValues[element].length == 0) {
+          delete this.filtrosValues[element];
+        }
+      });
+    }
+  }
+
   search(){
     this.progressSpinner = true;
-    this.remesasResultadosItem.descripcionRemesa = 'hola';
-    this.sigaServices.post("remesasResultados_buscarRemesasResultados", this.remesasResultadosItem).subscribe(
+    this.sigaServices.post("remesasResultados_buscarRemesasResultados", this.filtrosValues).subscribe(
       n => {
         console.log("Dentro del servicio que llama al buscarRemesasResultado");
         this.datos = JSON.parse(n.body).remesasResultadosItems;
