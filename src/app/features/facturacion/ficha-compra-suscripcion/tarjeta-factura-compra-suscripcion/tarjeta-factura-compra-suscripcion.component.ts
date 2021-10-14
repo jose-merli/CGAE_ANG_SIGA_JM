2973ca46f3e93d8dba3597b88ce59c37b50234c1
Guillermo Rejon <grejon@deloitte.es>
2021-10-14 13:57:17 +0200

    Se crea la tarjeta de facturacion en la ficha Compra/Suscripcion.

---
+++ src/app/features/facturacion/ficha-compra-suscripcion/tarjeta-factura-compra-suscripcion/tarjeta-factura-compra-suscripcion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tarjeta-factura-compra-suscripcion',
+  templateUrl: './tarjeta-factura-compra-suscripcion.component.html',
+  styleUrls: ['./tarjeta-factura-compra-suscripcion.component.scss']
+})
+export class TarjetaFacturaCompraSuscripcionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
