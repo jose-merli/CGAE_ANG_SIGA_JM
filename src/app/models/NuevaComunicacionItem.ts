f546443827f188115a357746ecede4987b783317
Guillermo Rejon <grejon@deloitte.es>
2021-10-20 15:39:33 +0200

    Modelo que es olvido incluir en el commit anterior

---
+++ src/app/models/NuevaComunicacionItem.ts
@@ -0,0 +1,10 @@
+export class NuevaComunicacionItem {
+    juzgado: string;
+    asunto: string;
+    fechaEfecto: Date;
+    mensaje: string;
+    numProcedimiento: string;
+    nig: string;
+    idClaseComunicacion: string;
+    docs: File[] = [];
+  }
