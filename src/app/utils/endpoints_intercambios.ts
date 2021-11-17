bb26078a3ac9e34fe9fd567374e88de04134310b
kvargasnunez <kvargasnunez@deloitte.es>
2021-11-17 17:54:33 +0100

    Modificado Endpoints Intercambios

--- src/app/utils/endpoints_intercambios.ts
+++ src/app/utils/endpoints_intercambios.ts
@@ -3,7 +3,11 @@ export let endpoints_intercambios: any = {
     intercambios_buscarCargaMasivaProcuradores: "cargaMasivaProcuradores/listado",
     intercambios_cargarFicheroCargaMasivaProcuradores: "cargaMasivaProcuradores/cargarFichero",
     intercambios_descargarFicheroCargaMasivaProcuradores: "cargaMasivaProcuradores/descargarFicheros",
+
     intercambios_buscarCargaDesignaProcuradores : "cargaDesignaProcuradores/buscarDesignaProcuradores",
+    intercambios_obtenerOperacionTipoAccion : "cargaDesignaProcuradores/obtenerOperacionTipoAccion",
+    intercambios_obtenerDesignaProcuradores : "cargaDesignaProcuradores/obtenerDesignaProcuradores",
     intercambios_contadorCargaDesignaProcuradores : "cargaDesignaProcuradores/recuperarDatosContador",
-    intercambios_obtenerDesignaProcuradores : "cargaDesignaProcuradores/obtenerDesignaProcuradores"
+    intercambios_guardarDesignaProcuradores : "cargaDesignaProcuradores/guardarDesignaProcuradores"
+    
 }
