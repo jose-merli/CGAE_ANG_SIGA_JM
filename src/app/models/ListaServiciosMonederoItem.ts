046e2c5b781fa149bb13ce86779d1b38db05bc6d
Guillermo Rejón <grejon@deloitte.es>
2021-12-07 12:17:07 +0100

    Creacion de la clase "ListaServiciosMonederoItem"

---
+++ src/app/models/ListaServiciosMonederoItem.ts
@@ -0,0 +1,6 @@
+export class ListaServiciosMonederoItem {
+    nombre: string; //Nombre del servicio
+	fecha: Date;//Fecha informativa en la que se añadió
+	precioPerio: string; //campo informativo que muestra lo que cuesta el servicio (mismo formato que la busqueda de servicios)
+
+}
