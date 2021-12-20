72c65017fb67bcd79a22e7ed326486d4840357cb
ogordonrodriguez <ogordonrodriguez@deloitte.es>
2021-12-20 11:56:43 +0100

    añadir item de facturas incluidas y endpoint al servicio de backend

--- src/app/utils/endpoints_facturacionPyS.ts
+++ src/app/utils/endpoints_facturacionPyS.ts
@@ -64,4 +64,5 @@ export let endpoints_facturacionPyS: any = {
     facturacionPyS_getEstadosPagos: "facturacionPyS/getEstadosPagos",
     facturacionPyS_insertarEstadosPagos: "facturacionPyS/insertarEstadosPagos",
     facturacionPyS_eliminarEstadosPagos: "facturacionPyS/eliminarEstadosPagos",
+    facturacionPyS_getFacturasIncluidas: "facturacionPyS/getFacturasIncluidas",
 }
