09351c6c338db6b24123761b49d67dca5ab4169e
fdelrosal <fdelrosal@deloitte.es>
2021-12-29 15:33:41 +0100

    [fdelrosal] boton descargar log de reintegros de la xunta

--- src/app/features/sjcs/facturacionSJCS/certificacion-fac/ficha-certificacion-fac/ficha-certificacion-fac.component.ts
+++ src/app/features/sjcs/facturacionSJCS/certificacion-fac/ficha-certificacion-fac/ficha-certificacion-fac.component.ts
@@ -22,6 +22,7 @@ import { TarjetaDatosGeneralesCertificacionComponent } from './tarjeta-datos-gen
 import { TarjetaFacturacionComponent } from './tarjeta-facturacion/tarjeta-facturacion.component';
 import { TarjetaMovimientosVariosAplicadosComponent } from './tarjeta-movimientos-varios-aplicados/tarjeta-movimientos-varios-aplicados.component';
 import { TarjetaMovimientosVariosAsociadosComponent } from './tarjeta-movimientos-varios-asociados/tarjeta-movimientos-varios-asociados.component';
+import { saveAs } from "file-saver/FileSaver";
 
 export interface Enlace {
   id: string;
@@ -422,4 +423,35 @@ export class FichaCertificacionFacComponent implements OnInit, AfterViewChecked 
     )
   }
 
+  descargarLogReintegrosXunta(event){
+    if(event == true){
+      this.progressSpinner = true;
+      let idFactsList: string[] =[];
+
+      if(!this.tarjetaFact.datos != null|| this.tarjetaFact.datos != undefined || this.tarjetaFact.datos.length !=0){
+        for(let idFact of this.tarjetaFact.datos){
+          idFactsList.push(idFact.idFacturacion.toString());
+        }
+  
+      this.sigaService.postDownloadFiles("certificaciones_descargarLogReintegrosXunta", idFactsList).subscribe(
+        data => {
+  
+          let blob = null;
+  
+            blob = new Blob([data], { type: "application/zip" });
+            saveAs(blob, "Reintegros_Xunta_Error_Log.zip");
+       
+          this.progressSpinner = false;
+        },
+        err => {
+          this.progressSpinner = false;
+        },
+        () => {
+          this.progressSpinner = false;
+        }
+      );
+      }
+    }
+      }
+   
 }
