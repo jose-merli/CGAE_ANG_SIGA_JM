2b0e5756515acff8af18722c49e0b03f61f1dba4
Alvaro <aguerrarodriguez@deloitte.es>
2022-02-25 12:02:12 +0100

    Cambios en los endpoints

--- src/app/features/facturacion/servicios/ficha-servicios/detalle-tarjeta-datos-generales-ficha-servicios-facturacion/detalle-tarjeta-datos-generales-ficha-servicios-facturacion.component.ts
+++ src/app/features/facturacion/servicios/ficha-servicios/detalle-tarjeta-datos-generales-ficha-servicios-facturacion/detalle-tarjeta-datos-generales-ficha-servicios-facturacion.component.ts
@@ -41,6 +41,7 @@ export class DetalleTarjetaDatosGeneralesFichaServiciosFacturacionComponent impl
   //Variables Dialog Borrar Suscripciones y bajas
   borrarSuscripcionBajaItem: BorrarSuscripcionItem = new BorrarSuscripcionItem;
   showModalSuscripcionesBajas = false; //Muestra o no muestra el dialogo de suscripciones o bajas
+  showModalServicioAutomaticoAManual: boolean = false;
   checkboxIncluirSolBajasManuales: boolean = false;
 
   //variables de control
@@ -452,23 +453,18 @@ export class DetalleTarjetaDatosGeneralesFichaServiciosFacturacionComponent impl
     this.showModalSuscripcionesBajas = false;
   }
 
-  showModalServicioAutomaticoAManual: boolean = false;
   modalServicioAutomaticoAManual(){
     let msg = this.commonsService.checkPermisos(this.permisoEliminarSuscripcionBaja, undefined);
 	    if (msg != null) {
 	      this.msgs = msg;
 	    } else {
-        this.showModalServicioAutomaticoAManual = true;;
+        this.showModalServicioAutomaticoAManual = true;
 	    }
   }
 
   cancelarDialogServicioAutomaticoAManual() {
     this.checkboxDialogServicioAutomaticoAManual = false;
     this.showModalServicioAutomaticoAManual = false;
-  }
-
-  onChangeDarDeBajaAtodasLasPersonas(){
-    
   }
 
   nuevacondicion(){
