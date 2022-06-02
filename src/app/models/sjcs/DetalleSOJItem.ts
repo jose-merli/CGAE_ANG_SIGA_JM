42b5372945bae455728f8e185e611797350605a1
Paco Mansilla <fmansilla@deloitte.es>
2022-06-03 01:07:14 +0200

    modifico la llamada a SOJ

---
+++ src/app/models/sjcs/DetalleSOJItem.ts
@@ -0,0 +1,18 @@
+export class DetalleSOJItem {
+  numeroSOJ: String;
+  IDTIPOSOJ: String;
+  ANIO: String;
+  idPersonaJG: String;
+  idInstitucionJG: String;
+  actionE: String;
+  tituloE: String;
+  conceptoE: String;
+  NUMERO: String;
+  anioSOJ: String;
+  localizacionE: String;
+  IDINSTITUCION: String;
+  idInstitucionSOJ: String;
+  idTipoSOJ: String;
+  accionE: String;
+  constructor() { }
+}
