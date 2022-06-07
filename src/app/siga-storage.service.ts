ec0763c066cad1c6ad12104cd7a5b1bf9f576187
Paco Mansilla <fmansilla@deloitte.es>
2022-06-07 16:50:58 +0200

    Cambio a versión 1.0.82_1

--- src/app/siga-storage.service.ts
+++ src/app/siga-storage.service.ts
@@ -8,7 +8,7 @@ export class SigaStorageService {
   numColegiado: any;
   nombreApe: string;
   institucionActual: string;
-  version: string = '1.0.82_0'
+  version: string = '1.0.82_1'
   constructor() { }
 
 }
