353439c6bfa57349a21102f94ffddb7add66651e
jgonzalezcerrato <jgonzalezcerrato@deloitte.es>
2022-08-26 15:20:18 +0200

    SIGARNV-2512 - No funciona la relación de un EJG con SOJ

--- src/app/features/sjcs/soj/detalle-soj/detalle-soj.component.ts
+++ src/app/features/sjcs/soj/detalle-soj/detalle-soj.component.ts
@@ -17,24 +17,25 @@ export class DetalleSOJComponent implements OnInit {
 
   constructor(public oldSigaServices: OldSigaServices, private location: Location, private router: Router) {
     //this.url = this.oldSigaServices.getOldSigaUrl('detalleSOJ');
-    this.url = JSON.parse(sessionStorage.getItem('url'));
-    sessionStorage.removeItem('url');
+    //this.url = JSON.parse(sessionStorage.getItem('url'));
+    //sessionStorage.removeItem('url');
    // this.url +='&anio=2018&desdeEJG=si&idInstitucion=2005&idTipoSOJ=2&modo=Editar&numero=922';
     //this.url +='&numeroSOJ=922&IDTIPOSOJ=2&ANIO=2018&idPersonaJG=552608&idInstitucionJG=2005&actionE=/JGR_PestanaSOJBeneficiarios.do&tituloE=pestana.justiciagratuitasoj.solicitante&conceptoE=SOJ&NUMERO=922&anioSOJ=2018&localizacionE=gratuita.busquedaSOJ.localizacion&IDINSTITUCION=2005&idTipoSOJ=2&idInstitucionSOJ=2005&accionE=editar';
-
     //console.log('url es:'+this.url);
-    
-
     if (sessionStorage.getItem('reload') == 'si') {
       sessionStorage.removeItem('reload');
       sessionStorage.setItem('reload', 'no');
-      this.url = JSON.parse(sessionStorage.getItem('url'));
-      sessionStorage.removeItem('url');
+      //this.url = JSON.parse(sessionStorage.getItem('url'));
+      //sessionStorage.removeItem('url');
       setTimeout(() => {
         this.url = JSON.parse(sessionStorage.getItem('url'));
+        sessionStorage.removeItem('url');
         document.getElementById('noViewContent').className = 'mainFrameWrapper';
         this.progressSpinner = false;
       }, 2000);
+    }else{
+      this.url = JSON.parse(sessionStorage.getItem('url'));
+      sessionStorage.removeItem('url');
     }
   }
 
