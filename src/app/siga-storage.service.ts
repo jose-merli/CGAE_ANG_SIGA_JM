3218a3b907adedf2c28c80e1fe671a28c17d3ef8
Francisco Mansilla de Sebastián <fmansilla@deloitte.es>
2023-06-07 14:56:57 +0200

    Actualizo versión 1.0.90_1

--- src/app/siga-storage.service.ts
+++ src/app/siga-storage.service.ts
@@ -8,7 +8,7 @@ export class SigaStorageService {
   numColegiado: any;
   nombreApe: string;
   institucionActual: string;
-  version: string = '1.0.90_0'
+  version: string = '1.0.90_1'
   constructor() { }
 
 }
