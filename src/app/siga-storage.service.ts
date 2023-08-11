0d4185d69107a5e99a1482745df0cf5552ef9511
Jesús Rivero <jriveromuniz@deloitte.es>
2023-08-11 12:56:18 +0200

    Actualizo versión 1.0.98_1

--- src/app/siga-storage.service.ts
+++ src/app/siga-storage.service.ts
@@ -8,7 +8,7 @@ export class SigaStorageService {
   numColegiado: any;
   nombreApe: string;
   institucionActual: string;
-  version: string = '1.0.98_0'
+  version: string = '1.0.98_1'
   constructor() { }
 
 }
