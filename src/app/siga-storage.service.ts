68fd6f365b211b021abec0579ec0361e98d754ff
José Antonio Martín Álvarez <jmartinalvarez@deloitte.es>
2023-08-23 11:10:26 +0200

    Actualizo versión 1.0.100_3

--- src/app/siga-storage.service.ts
+++ src/app/siga-storage.service.ts
@@ -8,7 +8,7 @@ export class SigaStorageService {
   numColegiado: any;
   nombreApe: string;
   institucionActual: string;
-  version: string = '1.0.100_2'
+  version: string = '1.0.100_3'
   constructor() { }
 
 }
