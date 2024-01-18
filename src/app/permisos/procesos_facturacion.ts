6da9b63a9239d666cc1b0df06b1aaab73e197144
Jesús Rivero <jriveromuniz@deloitte.es>
2024-01-18 16:50:42 +0100

    SIGARNV-3063 - Añado permisos restantes

--- src/app/permisos/procesos_facturacion.ts
+++ src/app/permisos/procesos_facturacion.ts
@@ -1,5 +1,12 @@
 export const procesos_facturacionSJCS: any = {
 
     facturacionYpagos: "600",
-    cartasFacturacionPago: "620"
+    cartasFacturacionPago: "620",
+    cartasFacturacion: "620F",
+    cartasPagos: "620P",
+    abonosSJCS: "60A",
+    facturas: "220",
+    monederos: "50",
+    cuotasSuscripciones: "45",
+    compraProductos: "307"
 }
