a3d4f6630c7e957afff7e500e50897880e761cec
José Ramón Gil García <jgilgarcia@deloitte.es>
2024-03-15 14:03:25 +0100

    SIGARNV-4062 recuperar todos los datos del EJG

--- src/app/features/sjcs/justiciables/gestion-justiciables/asuntos/asuntos.component.ts
+++ src/app/features/sjcs/justiciables/gestion-justiciables/asuntos/asuntos.component.ts
@@ -457,14 +457,14 @@ export class AsuntosComponent implements OnInit, OnChanges {
         let ejgItem = new EJGItem();
         ejgItem.annio = dato.anio;
         // ejgItem.numero = dato.numero;
-        ejgItem.numero = dato.asunto.substring(dato.asunto.indexOf('/')+1);
+        ejgItem.numero = dato.numero;
         ejgItem.idInstitucion = dato.idinstitucion;
         ejgItem.tipoEJG = dato.clave;
 
         let result;
         // al no poder obtener todos los datos del EJG necesarios para obtener su informacion
         //se hace una llamada a al base de datos pasando las claves primarias y obteniendo los datos necesarios
-        this.sigaServices.post("filtrosejg_busquedaEJG", ejgItem).subscribe(
+        this.sigaServices.post("gestionejg_datosEJG", ejgItem).subscribe(
           n => {
             result = JSON.parse(n.body).ejgItems;
             this.persistenceService.setDatosEJG(result[0]);
