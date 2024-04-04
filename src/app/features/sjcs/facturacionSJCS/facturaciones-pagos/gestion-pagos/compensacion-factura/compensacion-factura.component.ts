cb5da0fdfca4c18ba514b920a22c52bc9da684bd
Jesús Rivero <jriveromuniz@deloitte.es>
2024-04-04 11:36:11 +0200

    SIGARNV-3054 - Cambios para mostrar los importes de los pagos

--- src/app/features/sjcs/facturacionSJCS/facturaciones-pagos/gestion-pagos/compensacion-factura/compensacion-factura.component.ts
+++ src/app/features/sjcs/facturacionSJCS/facturaciones-pagos/gestion-pagos/compensacion-factura/compensacion-factura.component.ts
@@ -63,7 +63,7 @@ export class CompensacionFacturaComponent implements OnInit, AfterViewInit {
       this.getCols();
 
       if (this.modoEdicion && this.idEstadoPago == '20') {
-        this.getCompensacionFacturas(this.paramDeducirCobroAutom != undefined ? this.paramDeducirCobroAutom.valor.toString() : undefined);
+        this.getCompensacionFacturas(this.paramDeducirCobroAutom != undefined ? this.paramDeducirCobroAutom.toString() : undefined);
       }
 
     }).catch(error => console.error(error));
