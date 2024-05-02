d5af95431b8af6da2abbb01a569d8b2235d625ff
Antonio Rincón Ramos <anrinconramos@deloitte.es>
2024-05-02 12:01:53 +0200

    SIGANRV-4564: Se elimina el componente datos-direccion

--- src/app/features/sjcs/justiciables/justiciables.module.ts
+++ src/app/features/sjcs/justiciables/justiciables.module.ts
@@ -1,85 +1,45 @@
-import { NgModule, LOCALE_ID } from '@angular/core';
-import { CommonModule, DatePipe, UpperCasePipe, APP_BASE_HREF } from '@angular/common';
-import { BusquedaJusticiablesComponent } from './busqueda-justiciables/busqueda-justiciables.component';
-import { FiltroJusticiablesComponent } from './busqueda-justiciables/filtro-justiciables/filtro-justiciables.component';
-import { GestionJusticiablesComponent } from './gestion-justiciables/gestion-justiciables.component';
-import { DatosGeneralesComponent } from './gestion-justiciables/datos-generales/datos-generales.component';
-import { TablaJusticiablesComponent } from './busqueda-justiciables/tabla-justiciables/tabla-justiciables.component';
-import { DataTableModule, PaginatorModule, InputTextModule, ButtonModule, DropdownModule, CheckboxModule, GrowlModule, MenubarModule, MultiSelectModule, ConfirmationService } from 'primeng/primeng';
-import { FormsModule } from '@angular/forms';
-import { PipeTranslationModule } from '../../../commons/translate/pipe-translation.module';
-import { TableModule } from 'primeng/table';
-import { PrecioModule } from '../../../commons/precio/precio.module';
-import { ImagePipe } from '../../../commons/image-pipe/image.pipe';
-import { TrimPipePipe } from '../../../commons/trim-pipe/trim-pipe.pipe';
-import { SigaServices } from '../../../_services/siga.service';
-import { CommonsService } from '../../../_services/commons.service';
-import { cardService } from '../../../_services/cardSearch.service';
-import { HeaderGestionEntidadService } from '../../../_services/headerGestionEntidad.service';
-import { MessageService } from 'primeng/components/common/messageservice';
-import { AuthenticationService } from '../../../_services/authentication.service';
-import { AuthGuard } from '../../../_guards/auth.guards';
-import { environment } from '../../../../environments/environment';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { JwtInterceptor } from '../../../_interceptor/jwt.interceptor';
-import { CookieService } from 'ngx-cookie-service';
-import { FechaModule } from '../../../commons/fecha/fecha.module';
-import { routingJusticiables } from './justiciables-routing.module';
-import { DatosRepresentanteComponent } from './gestion-justiciables/datos-representante/datos-representante.component';
-import { FileUploadModule } from 'primeng/fileupload';
-import { AsuntosComponent } from './gestion-justiciables/asuntos/asuntos.component';
-import { DatosDireccionComponent } from './gestion-justiciables/datos-direccion/datos-direccion.component';
-import { DatosSolicitudComponent } from './gestion-justiciables/datos-solicitud/datos-solicitud.component';
-import { DialogModule } from 'primeng/dialog';
-import { ConfirmDialogModule } from 'primeng/primeng';
-import { DialogoModule } from '../../../commons/dialog/dialogo.module';
-import { DatosAbogadoContrarioComponent } from './gestion-justiciables/datos-abogado-contrario/datos-abogado-contrario.component';
-import { DatosProcuradorContrarioComponent } from './gestion-justiciables/datos-procurador-contrario/datos-procurador-contrario.component';
-import { DatosPersonalesComponent } from './gestion-justiciables/datos-personales/datos-personales.component';
-import { DatosUnidadFamiliarComponent } from './gestion-justiciables/datos-unidad-familiar/datos-unidad-familiar.component';
-import { TarjetaResumenFijaModule } from '../../../commons/tarjeta-resumen-fija/tarjeta-resumen-fija.module';
+import { APP_BASE_HREF, CommonModule, DatePipe, UpperCasePipe } from "@angular/common";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { LOCALE_ID, NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { CookieService } from "ngx-cookie-service";
+import { MessageService } from "primeng/components/common/messageservice";
+import { DialogModule } from "primeng/dialog";
+import { FileUploadModule } from "primeng/fileupload";
+import { ButtonModule, CheckboxModule, ConfirmDialogModule, ConfirmationService, DataTableModule, DropdownModule, GrowlModule, InputTextModule, MenubarModule, MultiSelectModule, PaginatorModule } from "primeng/primeng";
 import { RadioButtonModule } from "primeng/radiobutton";
+import { TableModule } from "primeng/table";
+import { environment } from "../../../../environments/environment";
+import { AuthGuard } from "../../../_guards/auth.guards";
+import { JwtInterceptor } from "../../../_interceptor/jwt.interceptor";
+import { AuthenticationService } from "../../../_services/authentication.service";
+import { cardService } from "../../../_services/cardSearch.service";
+import { CommonsService } from "../../../_services/commons.service";
+import { HeaderGestionEntidadService } from "../../../_services/headerGestionEntidad.service";
+import { SigaServices } from "../../../_services/siga.service";
+import { FechaModule } from "../../../commons/fecha/fecha.module";
+import { ImagePipe } from "../../../commons/image-pipe/image.pipe";
+import { PrecioModule } from "../../../commons/precio/precio.module";
+import { TarjetaResumenFijaModule } from "../../../commons/tarjeta-resumen-fija/tarjeta-resumen-fija.module";
+import { PipeTranslationModule } from "../../../commons/translate/pipe-translation.module";
+import { TrimPipePipe } from "../../../commons/trim-pipe/trim-pipe.pipe";
+import { BusquedaJusticiablesComponent } from "./busqueda-justiciables/busqueda-justiciables.component";
+import { FiltroJusticiablesComponent } from "./busqueda-justiciables/filtro-justiciables/filtro-justiciables.component";
+import { TablaJusticiablesComponent } from "./busqueda-justiciables/tabla-justiciables/tabla-justiciables.component";
+import { AsuntosComponent } from "./gestion-justiciables/asuntos/asuntos.component";
+import { DatosAbogadoContrarioComponent } from "./gestion-justiciables/datos-abogado-contrario/datos-abogado-contrario.component";
+import { DatosGeneralesComponent } from "./gestion-justiciables/datos-generales/datos-generales.component";
+import { DatosPersonalesComponent } from "./gestion-justiciables/datos-personales/datos-personales.component";
+import { DatosProcuradorContrarioComponent } from "./gestion-justiciables/datos-procurador-contrario/datos-procurador-contrario.component";
+import { DatosRepresentanteComponent } from "./gestion-justiciables/datos-representante/datos-representante.component";
+import { DatosSolicitudComponent } from "./gestion-justiciables/datos-solicitud/datos-solicitud.component";
+import { DatosUnidadFamiliarComponent } from "./gestion-justiciables/datos-unidad-familiar/datos-unidad-familiar.component";
+import { GestionJusticiablesComponent } from "./gestion-justiciables/gestion-justiciables.component";
+import { routingJusticiables } from "./justiciables-routing.module";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    routingJusticiables,
-    DataTableModule,
-    PaginatorModule,
-    InputTextModule,
-    ButtonModule,
-    DropdownModule,
-    CheckboxModule,
-    FormsModule,
-    GrowlModule,
-    PipeTranslationModule,
-    MenubarModule,
-    TableModule,
-    MultiSelectModule,
-    PrecioModule,
-    FechaModule,
-    FileUploadModule,
-    ConfirmDialogModule,
-    DialogModule,
-    TarjetaResumenFijaModule,
-    RadioButtonModule
-
-  ],
-  declarations: [
-    BusquedaJusticiablesComponent,
-    FiltroJusticiablesComponent,
-    TablaJusticiablesComponent,
-    GestionJusticiablesComponent,
-    DatosGeneralesComponent,
-    DatosRepresentanteComponent,
-    AsuntosComponent,
-    DatosSolicitudComponent,
-    DatosDireccionComponent,
-    DatosAbogadoContrarioComponent,
-    DatosProcuradorContrarioComponent,
-    DatosPersonalesComponent,
-    DatosUnidadFamiliarComponent
-  ],
+  imports: [CommonModule, routingJusticiables, DataTableModule, PaginatorModule, InputTextModule, ButtonModule, DropdownModule, CheckboxModule, FormsModule, GrowlModule, PipeTranslationModule, MenubarModule, TableModule, MultiSelectModule, PrecioModule, FechaModule, FileUploadModule, ConfirmDialogModule, DialogModule, TarjetaResumenFijaModule, RadioButtonModule],
+  declarations: [BusquedaJusticiablesComponent, FiltroJusticiablesComponent, TablaJusticiablesComponent, GestionJusticiablesComponent, DatosGeneralesComponent, DatosRepresentanteComponent, AsuntosComponent, DatosSolicitudComponent, DatosAbogadoContrarioComponent, DatosProcuradorContrarioComponent, DatosPersonalesComponent, DatosUnidadFamiliarComponent],
   providers: [
     // { provide: TranslationClass.TRANSLATIONS, useValue: TranslationClass.dictionary },
     ImagePipe,
@@ -97,17 +57,16 @@ import { RadioButtonModule } from "primeng/radiobutton";
     AuthGuard,
     {
       provide: APP_BASE_HREF,
-      useValue: environment.baseHref
+      useValue: environment.baseHref,
     },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: JwtInterceptor,
-      multi: true
+      multi: true,
     },
     CookieService,
-    { provide: LOCALE_ID, useValue: 'es-ES' }
+    { provide: LOCALE_ID, useValue: "es-ES" },
   ],
-  exports: [AsuntosComponent]
-
+  exports: [AsuntosComponent],
 })
-export class JusticiablesModule { }
+export class JusticiablesModule {}
